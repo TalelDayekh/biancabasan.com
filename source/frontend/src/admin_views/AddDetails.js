ae9f85d16db04de3ce99f9351152e0dca20988cb
taleldayekh <mail@talel.se>
2018-11-25 12:38:05 +0100

    createDetails function redirecting to/passing on id to AddImages

--- source/frontend/src/admin_views/AddDetails.js
+++ source/frontend/src/admin_views/AddDetails.js
@@ -8,6 +8,7 @@ class AddDetails extends Component {
         height: "",
         width: "",
         description: "",
+        artwork_object_id: this.props.location.state.artwork_object_id,
     }
 
 
@@ -16,15 +17,15 @@ class AddDetails extends Component {
             <div>
                 <form onSubmit={ this.createDetails }>
                     <p>Height</p>
-                    <input onChange={ this.updateDetails } value={ this.state.height } name="height"></input>
+                    <input onChange={ this.updateDetails } value={ this.state.height } name="height"/>
                     <br />
                     <br />
                     <p>Widht</p>
-                    <input onChange={ this.updateDetails } value={ this.state.width } name="width"></input>
+                    <input onChange={ this.updateDetails } value={ this.state.width } name="width"/>
                     <br />
                     <br />
                     <p>Description</p>
-                    <textarea onChange={ this.updateDetails } value={ this.state.description } name="description" cols="60" rows="15"></textarea>
+                    <textarea onChange={ this.updateDetails } value={ this.state.description } name="description" cols="60" rows="15"/>
                     <br />
                     <button type="submit">Save</button>
                 </form>
@@ -42,11 +43,16 @@ class AddDetails extends Component {
     
     createDetails = (event) => {
         axios.post('http://localhost:8000/artworks_details/', {
-            title_id: this.props.location.state.artwork_id,
+            title_id: this.state.artwork_object_id,
             height: this.state.height,
             width: this.state.width,
             description: this.state.description,
-        }).then(response => {console.log(response)});
+        }).then(() => {
+            this.props.history.push({
+                pathname: '/admin_add_images/',
+                state: { artwork_object_id: this.state.artwork_object_id }
+            })
+        });
         this.setState({
             height: "",
             width: "",
