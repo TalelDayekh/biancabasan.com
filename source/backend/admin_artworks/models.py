c37c5284902607246d6213c16d40435c3e6f118a
taleldayekh <mail@talel.se>
2018-11-27 20:56:55 +0100

    owner field added to ArtworkTitle model

--- source/backend/admin_artworks/models.py
+++ source/backend/admin_artworks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 
 """
 Artwork title
@@ -7,6 +9,8 @@ Artwork title
 class ArtworkTitle(models.Model):
     title = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    # Connect data to user
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title + str(' ') + str(self.id)
@@ -35,4 +39,4 @@ class ArtworkImages(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.images
+        return str(self.images)
