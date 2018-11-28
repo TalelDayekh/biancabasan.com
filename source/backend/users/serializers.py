e9f1aff39748e5de896b5c7d7e00d51cfd5b56ae
taleldayekh <mail@talel.se>
2018-11-28 14:31:19 +0100

    Create method with hashed password

--- source/backend/users/serializers.py
+++ source/backend/users/serializers.py
@@ -7,6 +7,19 @@ from django.contrib.auth.models import User
 
 
 class CreateUserSerializer(serializers.ModelSerializer):
+    # Set password field to write only
+    password = serializers.CharField(write_only = True)
+
     class Meta:
         model = User
-        fields = ('username', 'password', 'email')
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user_object = User(
+            username = validated_data['username'],
+            email = validated_data['email']
+        )
+        user_object.set_password(validated_data['password'])
+        user_object.save()
+
+        return validated_data
