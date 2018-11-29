e267c2b8e8facef04e55169d4d8f049a315dd1a4
taleldayekh <mail@talel.se>
2018-11-29 11:41:41 +0100

    AUTH_USER_MODEL in owner field + reorganizing of code

--- source/backend/admin_artworks/models.py
+++ source/backend/admin_artworks/models.py
@@ -1,42 +1,63 @@
 from django.db import models
 
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 """
 Artwork title
 """
 class ArtworkTitle(models.Model):
-    title = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length = 200)
+    date_added = models.DateTimeField(auto_now_add = True)
+    
     # Connect data to user
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+        )
+    
     def __str__(self):
-        return self.title + str(' ') + str(self.id)
+        return (
+            str('Title: ') 
+            + self.title + str(' ID: ') 
+            + str(self.id) + str(' Owner: ') 
+            + str(self.owner)
+            )
 
 
 """
 Artwork details
 """
 class ArtworkDetails(models.Model):
-    title_id = models.ForeignKey(ArtworkTitle, on_delete=models.CASCADE)
+    title_id = models.ForeignKey(
+        ArtworkTitle,
+        on_delete = models.CASCADE,
+    )
     height = models.FloatField()
     width = models.FloatField()
     description = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
-        return self.description
+        return (
+            str('Description: ') 
+            + self.description + str(' ID: ') 
+            + str(self.title_id.id) 
+            + str(' Owner: ') 
+            + str(self.title_id.owner)
+            )
 
 
-"""
-Artwork images
-"""
-class ArtworkImages(models.Model):
-    title_id = models.ForeignKey(ArtworkTitle, on_delete=models.CASCADE)
-    images = models.ImageField(upload_to='images')
-    date_added = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return str(self.images)
+
+
+# """
+# Artwork images
+# """
+# class ArtworkImages(models.Model):
+#     title_id = models.ForeignKey(ArtworkTitle, on_delete=models.CASCADE)
+#     images = models.ImageField(upload_to='images')
+#     date_added = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return str(self.images)
