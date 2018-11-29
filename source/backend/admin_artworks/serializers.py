845593bddef0e22c0cccc57ee323d79864734023
taleldayekh <mail@talel.se>
2018-11-29 11:55:38 +0100

    Reorganizing of code

--- source/backend/admin_artworks/serializers.py
+++ source/backend/admin_artworks/serializers.py
@@ -1,21 +1,39 @@
+"""
+admin_artworks serializers
+"""
 from rest_framework import serializers
 
-from .models import ArtworkTitle, ArtworkDetails, ArtworkImages
+from .models import ArtworkTitle, ArtworkDetails
 
 
+# Artwork title
 class ArtworkTitleSerializer(serializers.ModelSerializer):
     class Meta:
         model = ArtworkTitle
-        fields = ('id', 'title')
+        fields = (
+            'owner',
+            'id',
+            'title',
+        )
 
 
+# Artwork details
 class ArtworkDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ArtworkDetails
-        fields = ('id', 'title_id', 'height', 'width', 'description')
+        fields = (
+            'id',
+            'title_id',
+            'height',
+            'width',
+            'description',
+        )
 
 
-class ArtworkImagesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ArtworkImages
-        fields = ('id', 'title_id', 'images')
+
+
+
+# class ArtworkImagesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ArtworkImages
+#         fields = ('id', 'title_id', 'images')
