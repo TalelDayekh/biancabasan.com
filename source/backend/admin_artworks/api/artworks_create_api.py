1c5f5378dad60b3e81de4e4a0fa96bafaa1a2d59
taleldayekh <mail@talel.se>
2018-12-09 22:23:49 +0100

    Function based view for adding images w/ image manipulation

--- source/backend/admin_artworks/api/artworks_create_api.py
+++ source/backend/admin_artworks/api/artworks_create_api.py
@@ -1,11 +1,18 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from io import BytesIO
+
+from PIL import Image
 
 from .serializers import (
     ArtworkTitlesSerializer,
     ArtworkDetailsSerializer,
     ArtworkImagesSerializer,
+    AddImagesSerializer, # !! REMOVE !!
 )
 from ..models import (
     ArtworkTitles,
@@ -24,6 +31,7 @@ def artwork_create_title(request):
 
         if title_serialized.is_valid():
             title_serialized.save()
+
             return Response(
                 title_serialized.data,
                 status = status.HTTP_201_CREATED,
@@ -62,4 +70,51 @@ def artwork_create_details(request, id):
                 + artwork_title.title
                 + ', details already exist'),
                 status = status.HTTP_409_CONFLICT
+            )
+
+
+"""
+Add artwork images
+"""
+@api_view(['POST'])
+# Parser classes for accepting requests with various media types
+@parser_classes((MultiPartParser, FormParser))
+def artwork_add_images(request):
+
+    if request.method == 'POST':
+        image_serialized = AddImagesSerializer(data = request.data)
+
+        if image_serialized.is_valid():
+
+            """
+            Image manipulation
+            """
+            image = image_serialized.validated_data['image']
+            image_title = str(image)
+
+            # Load image from file
+            image_file = Image.open(image_serialized.validated_data['image'])
+
+            # Modify image
+            modified_image = image_file.resize((200, 200))
+            modified_image = modified_image.rotate(45)
+
+            # Save modified image to in memory buffer and
+            # change InMemoryUploadedFile to modified image
+            buffer = BytesIO()
+            modified_image.save(buffer, format = 'JPEG', quality = 10)
+            image_serialized.validated_data['image'] = InMemoryUploadedFile(
+                buffer,
+                None,
+                image_title,
+                None,
+                None,
+                None
+            )
+
+            image_serialized.save()
+
+            return Response (
+                image_serialized.data,
+                status = status.HTTP_201_CREATED
             )
