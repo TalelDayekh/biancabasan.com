0fb2bd27287cfa72a96ddc5650c5b675b60e5477
taleldayekh <mail@talel.se>
2018-12-23 18:34:47 +0100

    Updated url patterns

--- source/backend/admin_artworks/urls.py
+++ source/backend/admin_artworks/urls.py
@@ -1,18 +1,22 @@
 from django.urls import path, include
 
-from rest_framework import routers
-
 from . import views
 
 
 app_name = 'admin_artworks'
 
 
-router = routers.DefaultRouter()
-router.register('artwork_titles', views.ArtworkTitleViewSet)
-router.register('artworks_details', views.ArtworkDetailsViewSet)
-router.register('artworks_images', views.ArtworkImagesViewSet)
-
 urlpatterns = [
-    path('', include(router.urls))
+    # List artworks
+    path('artworks_list/', views.artworks_list),
+
+    # Create artworks
+    path('add_title/', views.artwork_create_title),
+    path('add_details/', views.artwork_create_details),
+    # ADD ID TO IMAGES PATH #
+    path('add_images/', views.artwork_add_images),
+
+    # Edit artworks
+    path('edit_title/<int:id>', views.artwork_edit_title),
+    path('edit_details/<int:id>', views.artwork_edit_details),
 ]
