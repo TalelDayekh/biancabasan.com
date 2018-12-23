07f6246019f569dc90b742085f6461f269cd5887
taleldayekh <mail@talel.se>
2018-12-23 18:50:29 +0100

    Initial commit

---
+++ source/frontend/src/ui/admin/AddImages.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class AddImages extends Component {
+
+    render = () => {
+        return(
+            <div>Add Images</div>
+        )
+    }
+}
+
+
+export default AddImages;
