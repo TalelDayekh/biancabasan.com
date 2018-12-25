0d9c1acbecbf1730034ae46e0e644a5cbfb244da
taleldayekh <mail@talel.se>
2018-12-25 01:06:18 +0100

    Initial commit

---
+++ source/frontend/src/ui/elements/Headings.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+export const AdminHeading = styled.p`
+    /* Text style */
+    font-family: 'Comfortaa', cursive;
+    text-align: center;
+    color: #FFF59D;
+    font-size: 24px;
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    line-height: 33px;
+
+    /* Heading position */
+    margin-top: 160px;
+`
