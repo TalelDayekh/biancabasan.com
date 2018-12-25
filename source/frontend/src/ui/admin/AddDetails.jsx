ab9362e2dea1aed87af889761c55a4ce677b2b5e
taleldayekh <mail@talel.se>
2018-12-25 14:24:25 +0100

    Added remaining form fields and refactored validation code

--- source/frontend/src/ui/admin/AddDetails.jsx
+++ source/frontend/src/ui/admin/AddDetails.jsx
@@ -6,8 +6,10 @@ import {
     AdminHeading,
     ShortInputField,
     LongInputField,
+    TextField,
+    SaveButton,
+    BackButton
 } from '../elements/';
-import { throws } from 'assert';
 
 
 class AddDetails extends Component {
@@ -15,13 +17,23 @@ class AddDetails extends Component {
     state = {
         yearFrom: "",
         yearTo: "",
+        material: "",
+        height: "",
+        width: "",
+        depth: "",
+        description: "",
         // Input errors
         incorrectInput: new Set()
     }
 
     // Variables for input field placeholders
-    yearFrom = "Created between YYYY";
-    yearTo = "and YYYY*";
+    yearFrom = "Created between YYYY*"
+    yearTo = "and YYYY*"
+    material = "Material*"
+    height = "Height*"
+    width = "Width*"
+    depth = "Depth"
+    description = "Description*"
 
 
     render = () => {
@@ -37,27 +49,106 @@ class AddDetails extends Component {
                     {/* Years */}
                     <div className = "years-flex-container">
                         <ShortInputField
-                            name = "yearFrom"
+                            id = "yearFrom"
+                            name = "years"
                             size = "1"
                             placeholder = { this.yearFrom }
                             onFocus = { this.clearPlaceholder }
-                            onBlur = { this.formValidation }
+                            onBlur = { this.inputValidation }
                             value = { this.state.yearFrom }
                             onChange = { this.updateDetailsState }
                             raiseError = { this.state.incorrectInput.has('yearFrom') ? true : undefined }
                         />
-                        <div className = "spacer-div-middle" />
+                        <div className = "spacer-div-middle"/>
                         <ShortInputField
-                            name = "yearTo"
+                            id = "yearTo"
+                            name = "years"
                             size = "1"
                             placeholder = { this.yearTo }
                             onFocus = { this.clearPlaceholder }
-                            onBlur = { this.formValidation }
+                            onBlur = { this.inputValidation }
                             value = { this.state.yearTo }
                             onChange = { this.updateDetailsState }
                             raiseError = { this.state.incorrectInput.has('yearTo') ? true : undefined }
                         />
                     </div>
+                    {/* Material */}
+                    <div className = "input-flex-container">
+                        <LongInputField
+                            id = "material"
+                            name = "text"
+                            size = "1"
+                            placeholder = { this.material }
+                            onFocus = { this.clearPlaceholder }
+                            onBlur = { this.inputValidation }
+                            value = { this.state.material }
+                            onChange = { this.updateDetailsState }
+                            raiseError = { this.state.incorrectInput.has('material') ? true : undefined }
+                        />
+                    </div>
+                    {/* Height */}
+                    <div className = "input-flex-container">
+                        <ShortInputField
+                            id = "height"
+                            name = "numbers"
+                            size = "1"
+                            placeholder = { this.height }
+                            onFocus = { this.clearPlaceholder }
+                            onBlur = { this.inputValidation }
+                            value = { this.state.height }
+                            onChange = { this.updateDetailsState }
+                            raiseError = { this.state.incorrectInput.has('height') ? true : undefined }
+                        />
+                        <div className = "spacer-div-right"/>
+                    </div>
+                    {/* Width */}
+                    <div className = "input-flex-container">
+                        <ShortInputField
+                            id = "width"
+                            name = "numbers"
+                            size = "1"
+                            placeholder = { this.width }
+                            onFocus = { this.clearPlaceholder }
+                            onBlur = { this.inputValidation }
+                            value = { this.state.width }
+                            onChange = { this.updateDetailsState }
+                            raiseError = { this.state.incorrectInput.has('width') ? true : undefined }
+                        />
+                        <div className = "spacer-div-right"/>
+                    </div>
+                    {/* Depth */}
+                    <div className = "input-flex-container">
+                        <ShortInputField
+                            id = "depth"
+                            name = "numbers"
+                            size = "1"
+                            placeholder = { this.depth }
+                            onFocus = { this.clearPlaceholder }
+                            onBlur = { this.inputValidation }
+                            value = { this.state.depth }
+                            onChange = { this.updateDetailsState }
+                            raiseError = { this.state.incorrectInput.has('depth') ? true : undefined }
+                        />
+                        <div className = "spacer-div-right"/>
+                    </div>
+                    {/* Description */}
+                    <div className = "input-flex-container">
+                        <TextField
+                            id = "description"
+                            name = "text"
+                            size = "1"
+                            placeholder = { this.description }
+                            onFocus = { this.clearPlaceholder }
+                            onBlur = { this.inputValidation }
+                            value = { this.state.description }
+                            onChange = { this.updateDetailsState }
+                            raiseError = { this.state.incorrectInput.has('description') ? true : undefined }
+                        />
+                    </div>
+
+                    {/* Buttons */}
+                    <SaveButton>Save</SaveButton>
+                    <BackButton>Back</BackButton>
                 </form>
             </AdminContentWrapper>
         )
@@ -66,10 +157,10 @@ class AddDetails extends Component {
 
     updateDetailsState = (event) => {
         this.setState({
-            [event.target.name]: event.target.value
+            [event.target.id]: event.target.value
         });
 
-        let inputError = event.target.name;
+        let inputError = event.target.id;
         this.state.incorrectInput.delete(inputError) 
     }
 
@@ -77,20 +168,30 @@ class AddDetails extends Component {
         event.target.placeholder = ""
     }
 
-    formValidation = (event) => {
-        if (event.target.name === 'yearFrom') {
-            if (this.state.yearFrom.length != 4 || isNaN(this.state.yearFrom)) {
-                let updatedSet = this.state.incorrectInput.add('yearFrom');
+    inputValidation = (event) => {
+        let inputId = event.target.id
+
+        if (event.target.name === 'years') {
+            if(this.state[inputId].length != 4 || isNaN(this.state[inputId])) {
+                let updatedSet = this.state.incorrectInput.add(inputId);
                 this.setState({
                     incorrectInput: updatedSet
                 });
                 event.target.placeholder = "PLACEHOLDER TEXT"
             }
         }
-
-        if (event.target.name === 'yearTo') {
-            if (this.state.yearTo.length != 4 || isNaN(this.state.yearTo)) {
-                let updatedSet = this.state.incorrectInput.add('yearTo');
+        if (event.target.name === 'numbers') {
+            if(this.state[inputId].length === 0 || isNaN(this.state[inputId])) {
+                let updatedSet = this.state.incorrectInput.add(inputId);
+                this.setState({
+                    incorrectInput: updatedSet
+                });
+                event.target.placeholder = "PLACEHOLDER TEXT"
+            }
+        }
+        if (event.target.name === 'text') {
+            if(this.state[inputId].length === 0) {
+                let updatedSet = this.state.incorrectInput.add(inputId);
                 this.setState({
                     incorrectInput: updatedSet
                 });
