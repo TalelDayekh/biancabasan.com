0d465a7b5bd210b7d2a42a961ae8d9f53e266c12
taleldayekh <mail@talel.se>
2018-12-25 17:51:46 +0100

    Input validation for title field

--- source/frontend/src/ui/admin/AddTitle.jsx
+++ source/frontend/src/ui/admin/AddTitle.jsx
@@ -1,115 +1,82 @@
 import React, { Component } from 'react';
-
 import axios from 'axios';
-
+import './Admin.css';
 import {
-    Link,
-    Redirect
-} from 'react-router-dom';
-
-import { AdminContentWrapper } from '../layouts/Wrappers';
-import { AdminHeading } from '../elements/Headings'
-import { LongInputField } from '../elements/InputFields';
-import { SaveButton, BackButton } from '../elements/Buttons';
-
-
-const headingStyle = {
-    marginTop: '160px'
-}
-
-const inputContainer = {
-    display: 'flex',
-    marginTop: '55px'
-}
-
-const saveButtonStyle = {
-    float: 'left',
-    marginTop: '22px'
-}
-
-const backButtonStyle = {
-    float: 'left',
-    margin: '22px 0px 0px 10px'
-}
+    AdminContentWrapper,
+    AdminHeading,
+    LongInputField,
+    SaveButton,
+    BackButton
+} from '../elements/';
 
 
 class AddTitle extends Component {
 
     state = {
-        toAddDetails: false,
         title: "",
-        artworkObjectId: ""
+        // Input error
+        incorrectInput: false
     }
-
 
     // Variable for input field placeholder
     title = "Title*"
 
 
     render = () => {
-
-        // Redirect to AddDetails component and pass state
-        // with id for the newly created artwork object
-        if (this.state.toAddDetails === true) {
-            return <Redirect to = {{
-                pathname: "/admin/add_details",
-                state: { artworkObjectId: this.state.artworkObjectId }
-            }} />
-        }
-
-
         return(
             <AdminContentWrapper>
                 {/* Heading */}
-                <AdminHeading style = { headingStyle }>
+                <AdminHeading>
                     Did you make a new piece? Cool! What's it called?
                 </AdminHeading>
 
-                <form onSubmit = { this.createTitle }>
+                <form>
                     {/* Input field */}
                     {/* Title */}
-                    <div style = { inputContainer }>
+                    <div className = "top-input-flex-container">
                         <LongInputField
+                            id = "title"
                             size = "1"
                             placeholder = { this.title }
                             onFocus = { this.clearPlaceholder }
-                            onBlur = { (event) => event.target.placeholder = this.title }
+                            onBlur = { this.inputValidation }
                             value = { this.state.title }
                             onChange = { this.updateTitleState }
+                            raiseError = { this.state.incorrectInput ? true : undefined }
                         />
                     </div>
 
                     {/* Buttons */}
-                    <SaveButton style = { saveButtonStyle } type = "submit">Save</SaveButton>
-                    <Link to = "/admin/">
-                        <BackButton style = { backButtonStyle }>Back</BackButton>
-                    </Link>
+                    <SaveButton>Save</SaveButton>
+                    <BackButton>Back</BackButton>
                 </form>
             </AdminContentWrapper>
         )
     }
 
 
+    updateTitleState = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value,
+            incorrectInput: false
+        })
+    }
+
     clearPlaceholder = (event) => {
         event.target.placeholder = ""
     }
 
-    updateTitleState = (event) => {
-        this.setState({
-            title: event.target.value
-        })
+    inputValidation = (event) => {
+        if (this.state.title.length === 0) {
+            this.setState({
+                incorrectInput: true
+            });
+            event.target.placeholder = "PLACEHOLDER TEXT"
+        }
     }
 
-    createTitle = (event) => {
-        // !! REMOVE HARDCODED OWNER FROM POST REQUEST !!
-        axios.post('http://localhost:8000/admin_artworks/add_title/', { owner: "1", title: this.state.title })
-        .then(response => {
-            this.setState({
-                artworkObjectId: response.data.id,
-                toAddDetails: true
-            })
-        });
-        event.preventDefault();
+    createTitle = () => {
+
     }
 
 }
