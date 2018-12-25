8d2552bbd278b075cd8a0e6532be29ce094f91d5
taleldayekh <mail@talel.se>
2018-12-25 14:25:07 +0100

    Buttons file as jsx

--- source/frontend/src/ui/elements/Buttons.jsx
+++ source/frontend/src/ui/elements/Buttons.jsx
@@ -1,31 +1,48 @@
 import styled from 'styled-components';
 
 
-export const SaveButton = styled.div`
+export const SaveButton = styled.button`
+    /* Button */
     width: 60px;
     height: 28px;
     background: #FFF59D;
-    border-radius: 40px;
-    font-family: 'Comfortaa', cursive;
-    text-align: center;
-    color: #3A425A;
-    font-size: 12px;
-    font-weight: 700;
-    letter-spacing: 0.1em;
-    line-height: 28px;
-`
-
-export const BackButton = styled.div`
-    width: 60px;
-    height: 28px;
     border: 2px solid #FFF59D;
     border-radius: 40px;
     box-sizing: border-box;
+
+    /* Button text */
     font-family: 'Comfortaa', cursive;
-    text-align: center;
-    color: #FFF59D;
+    color: #3A425A;
     font-size: 12px;
+    text-align: center;
     font-weight: 700;
     letter-spacing: 0.1em;
-    line-height: 27px;
+    line-height: 26px;
+
+    /* Button positioning */
+    float: left;
+    margin-top: 22px;
+`
+
+export const BackButton = styled.button`
+    /* Button */
+    width: 60px;
+    height: 28px;
+    background: transparent;
+    border: 2px solid #FFF59D;
+    border-radius: 40px;
+    box-sizing: border-box;
+
+    /* Button text */
+    font-family: 'Comfortaa', cursive;
+    color: #FFF59D;
+    font-size: 12px;
+    text-align: center;
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    line-height: 26px;
+
+    /* Button positioning */
+    float: left;
+    margin: 22px 0px 0px 10px;
 `
