19654dbbed3fd3f31fe198d901cb0167481912fa
taleldayekh <mail@talel.se>
2019-01-26 12:59:29 +0100

    Updated views and url patterns

--- source/backend/admin_artworks/urls.py
+++ source/backend/admin_artworks/urls.py
@@ -1,22 +1,16 @@
 from django.urls import path, include
-
 from . import views
 
 
 app_name = 'admin_artworks'
 
-
 urlpatterns = [
     # List artworks
-    path('artworks_list/', views.artworks_list),
+    path('all_artworks/', views.artworks_list),
 
-    # Create artworks
-    path('add_title/', views.artwork_create_title),
-    path('add_details/', views.artwork_create_details),
-    # ADD ID TO IMAGES PATH #
-    path('add_images/', views.artwork_add_images),
-
-    # Edit artworks
-    path('edit_title/<int:id>', views.artwork_edit_title),
-    path('edit_details/<int:id>', views.artwork_edit_details),
+    # Add artworks
+    # Add info
+    path('add_artwork_info/', views.create_artwork_info),
+    # Add images
+    path('add_artwork_images/', views.add_artwork_images)
 ]
--- source/backend/admin_artworks/views.py
+++ source/backend/admin_artworks/views.py
@@ -1,46 +1,20 @@
-from .api import (
-    artworks_list_api,
-    artworks_create_api,
-    artworks_edit_api
+from .api import(
+    list_artworks_endpoints,
+    create_artworks_endpoints
 )
 
 
-# Artworks list
+# Views for listing artworks
 def artworks_list(request):
-
-    all_artworks = artworks_list_api.artworks_list(request)
+    all_artworks = list_artworks_endpoints.artworks_list(request)
     return all_artworks
 
 
-# Artworks create
-# Create title
-def artwork_create_title(request):
+# Views for adding new artworks
+def create_artwork_info(request):
+    create_info = create_artworks_endpoints.create_artwork_info(request)
+    return create_info
 
-    create_title = artworks_create_api.artwork_create_title(request)
-    return create_title
-
-# Create details
-def artwork_create_details(request):
-
-    create_details = artworks_create_api.artwork_create_details(request)
-    return create_details
-
-# Add images
-def artwork_add_images(request):
-
-    add_image = artworks_create_api.artwork_add_images(request)
+def add_artwork_images(request):
+    add_image = create_artworks_endpoints.add_artwork_images(request)
     return add_image
-
-
-# Artworks edit
-# Edit title
-def artwork_edit_title(request, id):
-
-    edit_title = artworks_edit_api.artwork_edit_title(request, id)
-    return edit_title
-
-# Edit details
-def artwork_edit_details(request, id):
-
-    edit_details = artworks_edit_api.artwork_edit_details(request, id)
-    return edit_details
