98462e3e7e213f4b047fe3029ea5adee9c203838
taleldayekh <mail@talel.se>
2019-02-04 07:27:27 +0100

    Nav and Auth reducers added

--- source/frontend/src/reducers/index.jsx
+++ source/frontend/src/reducers/index.jsx
@@ -2,8 +2,12 @@
 import { combineReducers } from 'redux';
 // Local imports
 import Artwork from './AdminArtworkReducer.jsx';
+import Redirect from './NavReducer.jsx';
+import Auth from './AuthReducers.jsx';
 
 
 export default combineReducers({
-    Artwork
+    Artwork,
+    Redirect,
+    Auth
 })
