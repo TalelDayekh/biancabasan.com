8dfbd01bdb22f958968512508660337b77d220b9
taleldayekh <mail@talel.se>
2019-02-13 16:10:19 +0100

    Tidied up code and added edit mode to state

--- source/frontend/src/reducers/NavReducer.jsx
+++ source/frontend/src/reducers/NavReducer.jsx
@@ -1,33 +1,44 @@
 const defaultState = {
+    // Pages redirect
     toAdminMain: false,
     toAdminTitle: false,
     toAdminDetails: false,
-    toAdminImages: false
+    toAdminImages: false,
+    // Pages edit mode
+    editMode: false
 }
 
 
 const Redirect = (state = defaultState, action) => {
     switch (action.type) {
+        // Pages redirect
         case 'ADMIN_MAIN':
             return state = {
                 ...state,
-                toAdminMain: action.payload
+                toAdminMain: true
             };
         case 'ADMIN_TITLE':
             return state = {
                 ...state,
-                toAdminTitle: action.payload
+                toAdminTitle: true
             };
         case 'ADMIN_DETAILS':
             return state = {
                 ...state,
-                toAdminDetails: action.payload
+                toAdminDetails: true
             };
         case 'ADMIN_IMAGES':
             return state = {
                 ...state,
-                toAdminImages: action.payload
+                toAdminImages: true
             };
+        // Pages edit mode
+        case 'EDIT_MODE':
+            return state = {
+                ...state,
+                editMode: action.payload
+            };
+        // Reset- and default state
         case 'RESET_REDIRECT_STATE':
             return state = {
                 ...state,
