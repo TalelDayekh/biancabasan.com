a0d9d6e4f7c192ee44776a17fd037572ce0df3dd
taleldayekh <mail@talel.se>
2019-02-13 16:09:35 +0100

    Tidied up code and added edit mode dispatch

--- source/frontend/src/ui/admin/Admin.jsx
+++ source/frontend/src/ui/admin/Admin.jsx
@@ -7,11 +7,15 @@ import {
 } from 'react-router-dom';
 // Local imports
 import {
-    toggleRedirect,
     resetRedirect,
+    toggleRedirect,
+    toggleEditMode,
     setArtwork,
+    inputError,
+    loadArtworks,
     createArtwork
-} from '../../actions/'
+} from '../../actions/';
+import AdminMain from './AdminMain.jsx';
 import AdminTitle from './AdminTitle.jsx';
 import AdminDetails from './AdminDetails.jsx';
 import AdminImages from './AdminImages.jsx';
@@ -26,6 +30,7 @@ class Admin extends Component {
         return(
             <MainWrapper>
                 <h1>ADMIN</h1>
+                <Route path="/admin/main/" render={ () => (<AdminMain { ...this.props }/>) }/>
                 <Route path="/admin/admin_title/" render={ () => (<AdminTitle { ...this.props }/>) }/>
                 <Route path="/admin/admin_details/" render={ () => (<AdminDetails { ...this.props }/>) }/>
                 <Route path="/admin/admin_images" render={ () => (<AdminImages { ...this.props }/>) }/>
@@ -38,22 +43,28 @@ class Admin extends Component {
 const mapStateToProps = (state) => {
     return {
         redirect: state.Redirect,
-        retrieveArtwork: state.Artwork
+        retrieveArtworks: state.Artwork
     }
 }
 const mapDispatchToProps = (dispatch) => {
     return {
+        // Redirect
         resetRedirect: () => { dispatch(resetRedirect()) },
-        toggleRedirect: (redirect_id) => {
-            dispatch(toggleRedirect(redirect_id))
-        },
-        setArtwork: (e) => { 
-            dispatch(setArtwork(e.target.id, e.target.value))
+        toggleRedirect: (redirect_id) => { dispatch(toggleRedirect(redirect_id)) },
+        toggleEditMode: (activateEditMode, artworkId) => { dispatch(toggleEditMode(activateEditMode, artworkId)) },
+        // Artwork input data
+        setArtwork: (e) => {
+            dispatch(setArtwork(e.target.id, e.target.value));
+            dispatch(inputError(false));
         },
         uploadArtworkImages: (e) => {
             dispatch(setArtwork(e.target.id, e.dataTransfer.files));
-            e.preventDefault()
+            e.preventDefault();
         },
+        inputError: (error) => { dispatch(inputError(error)) },
+        // GET requests
+        loadArtworks: () => { dispatch(loadArtworks()) },
+        // POST requests
         createArtwork: () => { dispatch(createArtwork()) }
     }
 }
