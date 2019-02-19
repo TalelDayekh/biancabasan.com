6cb3090a11230f64aaea327071ae4a99583f1c3d
taleldayekh <mail@talel.se>
2019-02-19 19:42:00 +0100

    Started refactoring all state and dispatch to app component

--- source/frontend/src/App.js
+++ source/frontend/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 // Redux
-import { Provider } from 'react-redux';
-import store from './store.jsx';
+import { connect } from 'react-redux';
 // React Router
 import {
     BrowserRouter,
+    Switch,
     Route
 } from 'react-router-dom';
 // Local imports
+// Actions
+import {
+    loadArtworks
+} from './actions/';
+// Views
+import Home from './ui/views/Home.jsx';
+// Admin
 import PrivateRoute from './ui/elements/PrivateRoute.jsx';
 import AdminLogin from './ui/admin/AdminLogin.jsx';
 import Admin from './ui/admin/Admin.jsx';
@@ -15,19 +22,43 @@ import Admin from './ui/admin/Admin.jsx';
 
 class App extends Component {
 
+    componentDidMount = () => {
+        // Get artworks from database
+        this.props.loadArtworks();
+    }
+
     render = () => {
         return(
-            <Provider store = { store }>
-                <BrowserRouter>
-                    <React.Fragment>
-                        <Route path="/login/" component={ AdminLogin }/>
-                        <PrivateRoute path="/admin/" component={ Admin }/>
-                    </React.Fragment>
-                </BrowserRouter>
-            </Provider>
+            <BrowserRouter>
+                <Switch>
+                    {/* Views routes */}
+                    <Route exact path='/' render={ () => (<Home { ...this.props }/>) }/>
+                    {/* Admin routes */}
+                    <Route path='/login/' render={ () => (<AdminLogin { ...this.props }/>) }/>
+                    <PrivateRoute path='/admin/' render={ () => (<Admin { ...this.props }/>) }/>
+                </Switch>
+            </BrowserRouter>
         )
+    }
+
+}
+
+
+// Entire app state
+const mapStateToProps = (state) => {
+    return {
+        // Artwork related state
+        retrieveArtwork: state.Artwork
+    }
+}
+
+// All dispatch methods
+const mapDispatchToProps = (dispatch) => {
+    return {
+        // GET requests
+        loadArtworks: () => { dispatch(loadArtworks()) },
     }
 }
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
