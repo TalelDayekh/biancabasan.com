e288a946994d16961a33eb9fb1aba91f21c3aac1
taleldayekh <mail@talel.se>
2019-03-01 10:53:22 +0100

    Changed to use Redux from App component and added Modal wrapper

--- source/frontend/src/ui/admin/AdminLogin.jsx
+++ source/frontend/src/ui/admin/AdminLogin.jsx
@@ -1,73 +1,55 @@
-import React, { Component } from 'react';
-// Redux
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
 // React Router
 import {
     Redirect
-} from 'react-router-dom';
+} from 'react-router-dom'
 // Local imports
+// Layouts
 import {
-    userLogin
-} from '../../actions/';
+    ModalWrapper
+} from '../layouts/'
 
 
 class AdminLogin extends Component {
 
     constructor(props) {
-        super(props);
-        this.unameInput = React.createRef();
-        this.pswInput = React.createRef();
+        super(props)
+        this.usernameInput = React.createRef()
+        this.passwordInput = React.createRef()
     }
-
 
     render = () => {
         if (this.props.auth.token !== null) {
-            return <Redirect push to="/admin/"/>
+            return <Redirect push to='/admin/'/>
         }
 
         return(
-            <React.Fragment>
-                {
-                    this.props.auth.isLoading ?
-                    <h2>Loading...</h2> // !! CHANGE TO SPINNER
-                    :
-                    <form onSubmit={(e) => { 
-                        this.props.loginUser(
-                            e,
-                            this.unameInput.current.value,
-                            this.pswInput.current.value
-                            ) 
-                        }}>
+            <ModalWrapper>
+                { this.props.auth.userLoaded ?
+                    <h2>Spinner...</h2>
+                :
+                    <form onSubmit={(e) => {
+                        this.props.loginUser(this.usernameInput.current.value, this.passwordInput.current.value)
+                        e.preventDefault()
+                    }}>
                         <label>Username</label>
                         <br/>
-                        <input ref={ this.unameInput } type="text"/>
+                        <input ref={ this.usernameInput } type='text'/>
                         <br/>
                         <br/>
                         <label>Password</label>
                         <br/>
-                        <input ref={ this.pswInput } type="password"/>
+                        <input ref={ this.passwordInput } type='password'/>
                         <br/>
-                        <button type="submit">Login</button>
+                        <br/>
+                        <button type='submit'>Login</button>
                     </form>
                 }
-            </React.Fragment>
+            </ModalWrapper>
         )
     }
+
 }
 
 
-const mapStateToProps = (state) => {
-    return {
-        auth: state.Auth
-    }
-}
-
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        loginUser: (e, username, password) => { dispatch(userLogin(username, password)); e.preventDefault() }
-    }
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(AdminLogin);
+export default AdminLogin
