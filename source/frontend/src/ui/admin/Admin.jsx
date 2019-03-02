9c87357f8b3414531a52f164d9bf0af97a36ead2
taleldayekh <mail@talel.se>
2019-03-02 22:12:12 +0100

    Rewriting admin components

--- source/frontend/src/ui/admin/Admin.jsx
+++ source/frontend/src/ui/admin/Admin.jsx
@@ -1,75 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // Redux
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
 // React Router
 import {
     Route
-} from 'react-router-dom';
+} from 'react-router-dom'
 // Local imports
+// Actions
 import {
-    resetRedirect,
-    toggleRedirect,
-    toggleEditMode,
-    setArtwork,
-    editArtwork,
-    inputError,
-    loadArtworks,
-    createArtwork
-} from '../../actions/';
-import AdminMain from './AdminMain.jsx';
-import AdminTitle from './AdminTitle.jsx';
-import AdminDetails from './AdminDetails.jsx';
-import AdminImages from './AdminImages.jsx';
+    loadSortedArtworks
+} from '../../actions/'
+// Layouts
 import {
-    MainWrapper
-} from '../layouts/';
+    ModalWrapper
+} from '../layouts/'
+// Admin
+import AdminMain from './AdminMain.jsx'
 
 
 class Admin extends Component {
 
     render = () => {
         return(
-            <MainWrapper>
-                <h1>ADMIN</h1>
-                <Route path="/admin/main/" render={ () => (<AdminMain { ...this.props }/>) }/>
-                <Route path="/admin/admin_title/" render={ () => (<AdminTitle { ...this.props }/>) }/>
-                <Route path="/admin/admin_details/" render={ () => (<AdminDetails { ...this.props }/>) }/>
-                <Route path="/admin/admin_images" render={ () => (<AdminImages { ...this.props }/>) }/>
-            </MainWrapper>
+            <ModalWrapper>
+                <h1>Admin</h1>
+                { this.props.artworks.artworksLoading ?
+                    <h2>Spinner...</h2>
+                :
+                    <Route path='/admin/main' render={ () => (<AdminMain { ...this.props }/>) }/>
+                }
+            </ModalWrapper>
         )
     }
+
 }
 
 
 const mapStateToProps = (state) => {
     return {
-        redirect: state.Redirect,
-        retrieveArtworks: state.Artwork
+        admin: state.ArtworksAdmin
     }
 }
+
 const mapDispatchToProps = (dispatch) => {
     return {
-        // Redirect
-        resetRedirect: () => { dispatch(resetRedirect()) },
-        toggleRedirect: (redirect_id) => { dispatch(toggleRedirect(redirect_id)) },
-        toggleEditMode: (activateEditMode, artworkId) => { dispatch(toggleEditMode(activateEditMode, artworkId)) },
-        // Artwork input data
-        setArtwork: (e) => {
-            dispatch(setArtwork(e.target.id, e.target.value));
-            dispatch(inputError(false));
-        },
-        uploadArtworkImages: (e) => {
-            dispatch(setArtwork(e.target.id, e.dataTransfer.files));
-            e.preventDefault();
-        },
-        editArtwork: (artworkToEdit) => { dispatch(editArtwork(artworkToEdit)) },
-        inputError: (error) => { dispatch(inputError(error)) },
-        // GET requests
-        loadArtworks: () => { dispatch(loadArtworks()) },
-        // POST requests
-        createArtwork: () => { dispatch(createArtwork()) }
+        // Artwork retrieve data
+        loadSortedArtworks: () => { dispatch(loadSortedArtworks()) }
     }
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Admin);
+export default connect(mapStateToProps, mapDispatchToProps)(Admin)
--- source/frontend/src/ui/admin/AdminMain.jsx
+++ source/frontend/src/ui/admin/AdminMain.jsx
@@ -1,51 +1,21 @@
-import React, { Component } from 'react';
-// React Router
-import {
-    Link
-} from 'react-router-dom';
+import React, { Component } from 'react'
 
 
 class AdminMain extends Component {
 
     componentDidMount = () => {
-        this.props.loadArtworks();
+        this.props.loadSortedArtworks()
     }
+
     render = () => {
         return(
-            <div>
-                {
-                    (this.props.retrieveArtworks.requestLoaded === false) ?
-                    <h2>Spinner...</h2>
-                    :
-                    <ul>
-                        {Object.keys(this.props.retrieveArtworks.artworksList).map((yearsKey, yearsIndex) => {
-                            const years = yearsKey
-                            return <li key={ yearsIndex }>{ years }
-                            <ul>
-                                {this.props.retrieveArtworks.artworksList[years].map((artworksKey, artworksIndex) => {
-                                    const artworkTitle = artworksKey.title
-                                    const artworkId = artworksKey.id
-                                    return <li key={ artworksIndex }>
-                                        <Link 
-                                            to="/admin/admin_title/"
-                                            onClick={ () => { this.editMode(artworkId) } }
-                                        >{ artworkTitle }</Link>
-                                    </li>
-                                })}
-                            </ul>
-                            </li>
-                        })}
-                    </ul>
-                }
-            </div>
+            <React.Fragment>
+                <h4>Admin Main</h4>
+            </React.Fragment>
         )
     }
 
-
-    editMode = (artworkId) => {
-        this.props.toggleEditMode(true, artworkId);
-    }
 }
 
 
-export default AdminMain;
+export default AdminMain
