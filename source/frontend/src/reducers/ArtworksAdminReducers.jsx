95f9bcf533151611781e607d14caf448f4212632
taleldayekh <mail@talel.se>
2019-03-12 06:55:32 +0100

    Rewritten reducers for artworks admin and removed old actions code

---
+++ source/frontend/src/reducers/ArtworksAdminReducers.jsx
@@ -0,0 +1,92 @@
+const defaultState = {
+    allArtworksSorted: {},
+    artworksSorting: true,
+    // Toggle edit mode
+    editMode: false,
+    // User input
+    inputErrors: [],
+    formError: false,
+    title: '',
+    yearFrom: '',
+    yearTo: '',
+    material: '',
+    height: '',
+    width: '',
+    depth: '',
+    description: ''
+}
+
+
+const ArtworksAdmin = (state = defaultState, action) => {
+    switch (action.type) {
+
+        case 'LIST_ALL_ARTWORKS_SORTED':
+            return state = {
+                ...state,
+                allArtworksSorted: action.payload
+            }
+        case 'SORT_SUCCESS':
+            return state = {
+                ...state,
+                artworksSorting: false
+            }
+        
+        // User input
+        case 'SET_INPUT_ERRORS':
+            return state = {
+                ...state,
+                inputErrors: action.payload
+            }
+        case 'SET_FORM_ERROR':
+            return state = {
+                ...state,
+                formError: action.payload
+            }
+        case 'SET_TITLE':
+            return state = {
+                ...state,
+                title: action.payload
+            }
+        case 'SET_YEAR_FROM':
+            return state = {
+                ...state,
+                yearFrom: action.payload
+            }
+        case 'SET_YEAR_TO':
+            return state = {
+                ...state,
+                yearTo: action.payload
+            }
+        case 'SET_MATERIAL':
+            return state = {
+                ...state,
+                material: action.payload
+            }
+        case 'SET_HEIGHT':
+            return state = {
+                ...state,
+                height: action.payload
+            }
+        case 'SET_WIDTH':
+            return state = {
+                ...state,
+                width: action.payload
+            }
+        case 'SET_DEPTH':
+            return state = {
+                ...state,
+                depth: action.payload
+            }
+        case 'SET_DESCRIPTION':
+            return state = {
+                ...state,
+                description: action.payload
+            }
+
+        default:
+            return state
+    }
+}
+
+
+export default ArtworksAdmin
