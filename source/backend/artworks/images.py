a3241b490976b5ada47bfc767692b3c2e4a6f4b3
taleldayekh <mail@talel.se>
2019-04-16 09:33:32 +0200

    artwork_title conversion using RegEx

--- source/backend/artworks/images.py
+++ source/backend/artworks/images.py
@@ -1,11 +1,8 @@
-# os
+import re
 import os
 
 
-class ArtworkImagesHandler():
-    """
-    Handles image paths and image manipulations
-    """
+class ImgPathHandler():
     def __init__(self, img_file, artwork_title):
         self.img_file = img_file
         self.artwork_title = artwork_title
@@ -15,18 +12,14 @@ class ArtworkImagesHandler():
         except AttributeError as err:
             print(err)
         else:
-            # Convert artwork_title to all lowercase
-            # and replace whitespace with underscore.
-            self.artwork_title = self.artwork_title.lower()
-            self.artwork_title = self.artwork_title.replace(' ', '_')
-
+            # Remove characters that are not letters or numbers from
+            # artwork_title, convert remaining ones to lowercase and
+            # replace whitespaces with underscores.
+            self.artwork_title = re.sub(
+                '[^A-Za-z0-9\s]{1}', '', self.artwork_title).replace(
+                ' ', '_').lower()
+            
             self.initial_img_path = os.path.dirname(self.img_file.path)
             self.new_img_path = os.path.join(
-                self.initial_img_path, self.artwork_title)
-
-    def mkdir_from_artwork_title(self):
-        if os.path.exists(self.new_img_path):
-            pass
-        else:
-            os.chdir(self.initial_img_path)
-            os.mkdir(self.artwork_title)
+                self.initial_img_path, self.artwork_title
+            )
