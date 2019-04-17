3e42e3a910d6833b40efdcbf4719496af6b8c151
taleldayekh <mail@talel.se>
2019-04-17 14:17:45 +0200

    ImgManipulationsHandler logic and unit test

--- source/backend/artworks/img_handlers.py
+++ source/backend/artworks/img_handlers.py
@@ -1,14 +1,15 @@
-import re
 import os
+import re
+from PIL import Image
 
 
 class ImgPathHandler():
-    def __init__(self, img_file, artwork_title):
-        self.img_file = img_file
-        self.artwork_title = artwork_title
+    def __init__(self, artwork_img_obj, artwork_title):
+        self.artwork_img_obj = artwork_img_obj
+        self.artwork_title = str(artwork_title)
 
         try:
-            self.img_file.path
+            self.artwork_img_obj.path
         except AttributeError as err:
             print(err)
         else:
@@ -19,7 +20,7 @@ class ImgPathHandler():
                 '[^A-Za-z0-9\s]{1}', '', self.artwork_title).replace(
                 ' ', '_').lower()
             
-            self.initial_img_path = os.path.dirname(self.img_file.path)
+            self.initial_img_path = os.path.dirname(self.artwork_img_obj.path)
             self.new_img_path = os.path.join(
                 self.initial_img_path, self.artwork_title
             )
@@ -35,4 +36,19 @@ class ImgPathHandler():
 
 
 class ImgManipulationsHandler():
-    pass
+    def __init__(self, img_file, new_img_width):
+        self.img_file = img_file
+        self.new_img_width = int(new_img_width)
+
+    def resize_img_proportionally(self):
+        try:
+            self.img_file.verify
+        except Exception:
+            print('Not a valid image file')
+        else:
+            img_width, img_height = self.img_file.size
+            new_img_height = (img_height/img_width) * self.new_img_width
+            resized_img = self.img_file.resize(
+                (self.new_img_width, int(round(new_img_height)))
+            )
+            return resized_img
--- source/backend/artworks/tests/test_img_handlers.py
+++ source/backend/artworks/tests/test_img_handlers.py
@@ -4,13 +4,13 @@ from django.test import TestCase, override_settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from PIL import Image
 from ..models import ArtworkImages
-from ..img_handlers import ImgPathHandler
+from ..img_handlers import ImgPathHandler, ImgManipulationsHandler
 
 
 def create_temp_test_img_file(img_name, img_file_format):
     temp_img_file = tempfile.NamedTemporaryFile()
-    image = Image.new('RGB', size=(1200, 1200), color=0)
-    image.save(temp_img_file, format=f'{img_file_format}')
+    img = Image.new('RGB', size=(1200, 950), color=0)
+    img.save(temp_img_file, format=f'{img_file_format}')
     temp_img_file.name = f'{img_name}' + '.' + f'{img_file_format}'
     temp_img_file.seek(0)
     return temp_img_file
@@ -37,18 +37,18 @@ class TestImgPathHandler(TestCase):
     @classmethod
     def setUpTestData(cls):
         test_img = create_temp_test_img_file('starry_night', 'jpeg')
-        uploaded_img = create_artwork_img_obj(test_img)
+        artwork_img_obj = create_artwork_img_obj(test_img)
 
         cls.img_path_handler_obj = ImgPathHandler(
-            uploaded_img,
+            artwork_img_obj,
             '#sTArrY niGHT 1!8.,8?9;  (vInCEnt: vAN GÖogH)'
         )
         cls.artwork_title = cls.img_path_handler_obj.artwork_title
         cls.initial_img_path = cls.img_path_handler_obj.initial_img_path
         cls.new_img_path = cls.img_path_handler_obj.new_img_path
 
-        print('Temporary test directories and files are being created at path:\n' 
-            + uploaded_img.path)
+        print('\nTemporary test directories and files are being created at path:\n' 
+            + artwork_img_obj.path)
     
     def test_artwork_title_formatting(self):
         self.assertEqual(
@@ -66,3 +66,19 @@ class TestImgPathHandler(TestCase):
         os.chdir(new_dir_path)
 
         self.assertEqual(os.getcwd(), self.new_img_path)
+
+
+class TestImgManipulationsHandler(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        created_test_img = create_temp_test_img_file('starry_night', 'jpeg')
+        cls.test_img = Image.open(created_test_img)
+
+    def test_resize_img_proportionally(self):
+        resized_img = ImgManipulationsHandler(
+            self.test_img,
+            1024
+        ).resize_img_proportionally()
+
+        self.assertEqual(resized_img.width, 1024)
+        self.assertEqual(resized_img.height, 811)
