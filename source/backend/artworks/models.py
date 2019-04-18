16b06c535d7420077f393d3c4db5154288c78791
taleldayekh <mail@talel.se>
2019-04-18 07:46:15 +0200

    Temporarily deleted fields and added related_name

--- source/backend/artworks/models.py
+++ source/backend/artworks/models.py
@@ -1,24 +1,14 @@
-# Models
 from django.db import models
-from django.conf import settings
 
 
 class ArtworkDetails(models.Model):
-    """
-    Provide all details for an artwork
-    """
-    owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.CASCADE
-        )
     title = models.CharField(max_length=200)
-    from_year = models.IntegerField()
-    to_year = models.IntegerField()
-    materials = models.CharField(max_length=500)
-    height = models.FloatField()
-    width = models.FloatField()
-    depth = models.FloatField(null=True, blank=True)
-    description = models.TextField()
 
-    def __str__(self):
-        return(str(self.id) + ' ' + self.title)
+
+class ArtworkImages(models.Model):
+    artwork_details = models.ForeignKey(
+        ArtworkDetails,
+        related_name='images',
+        on_delete=models.CASCADE
+    )
+    img = models.ImageField(upload_to='images')
