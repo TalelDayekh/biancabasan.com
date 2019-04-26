a5fd7260c1fa38dce61d3aa520c0a7dfb7e1bd61
taleldayekh <mail@talel.se>
2019-04-26 12:29:32 +0200

    Endpoints for adding artwork details and images

--- source/backend/api/urls.py
+++ source/backend/api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('artworks_list/', views.artworks_list)
+    path('api/artworks_list/', views.artworks_list),
+    path('api/artworks_add_details/', views.create_artwork_details),
+    path('api/artworks_add_images/', views.create_artwork_images)
 ]
