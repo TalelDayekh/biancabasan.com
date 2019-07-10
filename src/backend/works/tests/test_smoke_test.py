c0f662181b196b4a020165899762e57c4006f0ab
taleldayekh <mail@talel.se>
2019-07-10 09:29:06 +0200

    Formatted with Black

--- src/backend/works/tests/test_smoke_test.py
+++ src/backend/works/tests/test_smoke_test.py
@@ -6,6 +6,4 @@ from ..image_handler import smoke_test
 class InitialTest(TestCase):
     def test_smoke_test(self):
         hello_world = smoke_test()
-        self.assertEqual(
-            hello_world, "Failed Test"
-            )
+        self.assertEqual(hello_world, "Failed Test")
