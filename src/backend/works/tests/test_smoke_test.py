57f79966bb8088cefb85317187c0a4b6ceaf92bd
taleldayekh <mail@talel.se>
2019-07-10 16:10:51 +0200

    Travis CI test

--- src/backend/works/tests/test_smoke_test.py
+++ src/backend/works/tests/test_smoke_test.py
@@ -1,9 +1,13 @@
+from ..image_handler import smoke_test
 from django.test import TestCase
 
-from ..image_handler import smoke_test
+
 
 
 class InitialTest(TestCase):
     def test_smoke_test(self):
         hello_world = smoke_test()
-        self.assertEqual(hello_world, "Failed Test")
+        self.assertEqual(
+            hello_world,
+            "Failed Test"
+            )
