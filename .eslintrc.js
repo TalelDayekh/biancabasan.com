123274de5cce367614ac3eee3305588f7bd55753
taleldayekh <mail@talel.se>
2019-07-13 21:56:19 +0200

    ESLint and Prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+        jest: true
+    },
+    extends: [
+        'airbnb',
+        'plugin:react/recommended',
+        'prettier',
+        'plugin:prettier/recommended'
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018
+    },
+    plugins: ['react'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['warn', 'single'],
+        'prettier/prettier': 'error'
+    }
+};
