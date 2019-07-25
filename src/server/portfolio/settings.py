c863d82f9e40b32c8779da4f21f81cdc214dc453
taleldayekh <mail@talel.se>
2019-07-25 07:48:46 +0200

    Settings for handling static media files

--- src/server/portfolio/settings.py
+++ src/server/portfolio/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Local apps
     'users',
+    'works',
 ]
 
 MIDDLEWARE = [
