cf4173d9c820a32ce5223d03ba465ae218ae05e3
taleldayekh <mail@talel.se>
2019-07-26 19:54:46 +0200

    Create new dir from work title method and test

--- src/server/works/image_handlers.py
+++ src/server/works/image_handlers.py
@@ -1,2 +1,14 @@
-def smoke_test():
-    return "Hello World"
+from pathlib import Path
+
+from django.conf import settings
+
+
+class ImagePathHandler:
+    def __init__(self, work_title: str) -> None:
+        self.media_root = settings.MEDIA_ROOT
+        self.work_title = work_title
+
+    def _create_directory_from_work_title(self) -> Path:
+        work_title_directory = Path(self.media_root).joinpath(self.work_title)
+        work_title_directory.mkdir(parents=True, exist_ok=True)
+        return work_title_directory
