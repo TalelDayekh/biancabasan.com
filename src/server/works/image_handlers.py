04ce8bb1f180e86f1192aee88476f68b66850e7f
taleldayekh <mail@talel.se>
2019-07-27 10:20:05 +0200

    Method to format work title and test

--- src/server/works/image_handlers.py
+++ src/server/works/image_handlers.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 from django.conf import settings
@@ -7,8 +8,22 @@ class ImagePathHandler:
     def __init__(self, work_title: str) -> None:
         self.media_root = settings.MEDIA_ROOT
         self.work_title = work_title
+        self.formatted_work_title = self._format_work_title()
+
+    def _format_work_title(self) -> str:
+        # Removes characters which are not letters or numbers
+        # from a work title and replaces all whitespaces with
+        # underscores and converts the string into lowercase.
+        formatted_work_title = (
+            re.sub("[^A-Za-z0-9\s]{1}", "", self.work_title)
+            .replace(" ", "_")
+            .lower()
+        )
+        return formatted_work_title
 
     def _create_directory_from_work_title(self) -> Path:
-        work_title_directory = Path(self.media_root).joinpath(self.work_title)
+        work_title_directory = Path(self.media_root).joinpath(
+            self.formatted_work_title
+        )
         work_title_directory.mkdir(parents=True, exist_ok=True)
         return work_title_directory
