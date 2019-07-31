5422aa50fba30b43e7323a04a0a9ab9aa2977bda
taleldayekh <mail@talel.se>
2019-07-31 20:03:30 +0200

    Method to get image file size and refactoring

--- src/server/works/image_handlers.py
+++ src/server/works/image_handlers.py
@@ -4,6 +4,7 @@ import os
 import re
 import shutil
 import uuid
+from math import ceil
 from pathlib import Path
 from typing import Optional
 
@@ -19,21 +20,37 @@ class ImageHandler:
     def __init__(
         self, image_file: Path, work_title: Optional[str] = None
     ) -> None:
-        self.media_root = settings.MEDIA_ROOT
-        self.image_file = image_file
-        self.work_title_directory_name = work_title
+        self.image_file = None
+        try:
+            if not image_file.is_file():
+                raise OSError(
+                    "Path provided is not a path to a valid image file"
+                )
+            else:
+                self.image_file = image_file
+        except Exception as err:
+            print(err)
+
+        self.work_title = work_title
+        self.work_title_directory_name = None
         self.work_title_directory_path = None
+
+        self.media_root = settings.MEDIA_ROOT  # !! MOVE
 
     @property
     def image_file_size(self) -> int:
-        pass
+        try:
+            # Returns size in byte
+            return ceil(self.image_file.stat().st_size / 1024)
+        except AttributeError:
+            print("Can not get size of non image file")
 
     def _format_work_title(self) -> str:
         # Removes characters which are not letters or numbers
         # from a work title and replaces all whitespaces with
         # underscores and converts the string into lowercase.
         formatted_work_title = (
-            re.sub("[^A-Za-z0-9\s]{1}", "", self.work_title_directory_name)
+            re.sub("[^A-Za-z0-9\s]{1}", "", self.work_title)
             .replace(" ", "_")
             .lower()
         )
@@ -48,12 +65,10 @@ class ImageHandler:
 
     def move_image_to_work_title_directory(self) -> None:
         try:
-            if not self.work_title_directory_name:
+            if not self.work_title:
                 raise ValueError(
                     "A work title has to be provided for moving images to a new directory"
                 )
-            elif not self.image_file.is_file():
-                raise OSError("Path is not a valid image file")
             else:
                 self._format_work_title()
                 self._create_directory_from_work_title()
@@ -66,8 +81,8 @@ class ImageHandler:
     def delete_image(self) -> None:
         try:
             os.remove(self.image_file)
-        except FileNotFoundError as err:
-            print("No such image file to delete")
+        except TypeError as err:
+            print("No image file to delete")
 
     @classmethod
     def create_web_images_set(
@@ -75,7 +90,9 @@ class ImageHandler:
     ) -> List[ImageHandler]:
         try:
             if not image_file.is_file():
-                raise OSError("Path is not a valid image file")
+                raise OSError(
+                    "Path provided is not a path to a valid image file"
+                )
             else:
                 uid = str(uuid.uuid4().hex)
                 web_images = [
--- src/server/works/tests/test_image_handlers.py
+++ src/server/works/tests/test_image_handlers.py
@@ -9,64 +9,67 @@ from PIL import Image
 
 from ..image_handlers import ImageHandler
 
-image_path_handler_temp_media_directory = tempfile.mkdtemp(
+image_handler_temp_media_directory = tempfile.mkdtemp(
     prefix="image_path_handler_test_directory"
 )
 
 
-@override_settings(MEDIA_ROOT=image_path_handler_temp_media_directory)
+@override_settings(MEDIA_ROOT=image_handler_temp_media_directory)
 class ImageHandlerTest(TestCase):
     def setUp(self):
-        self.image_file = Path(
-            image_path_handler_temp_media_directory
-        ).joinpath("black_square.jpg")
+        self.image_file = Path(image_handler_temp_media_directory).joinpath(
+            "black_square.jpg"
+        )
         test_image = Image.new("RGB", (500, 500))
         test_image.save(self.image_file)
         self.non_existing_image_file = Path("/path/to/non/existing/image")
-        self.image_path_handler_instance = ImageHandler(
+        self.image_handler_inst = ImageHandler(
             self.image_file, "#bLAÄck SQuaRe, 20!.19;  "
         )
 
+    def test_can_get_image_file_size(self):
+        image_file_size = self.image_handler_inst.image_file_size
+        self.assertEqual(image_file_size, 5)
+
+    def test_can_not_get_image_file_size(self):
+        image_file_size = ImageHandler(
+            self.non_existing_image_file, ""
+        ).image_file_size
+        self.assertEqual(image_file_size, None)
+
     def test_can_format_work_title(self):
-        self.image_path_handler_instance._format_work_title()
+        self.image_handler_inst._format_work_title()
         self.assertEqual(
-            self.image_path_handler_instance.work_title_directory_name,
+            self.image_handler_inst.work_title_directory_name,
             "black_square_2019__",
         )
 
     def test_can_create_directory_from_work_title(self):
-        self.image_path_handler_instance._create_directory_from_work_title()
+        self.image_handler_inst._format_work_title()
+        self.image_handler_inst._create_directory_from_work_title()
         self.assertTrue(
-            Path(
-                self.image_path_handler_instance.work_title_directory_path
-            ).exists()
+            Path(self.image_handler_inst.work_title_directory_path).exists()
         )
 
     def test_can_move_image_to_work_title_directory(self):
-        self.image_path_handler_instance.move_image_to_work_title_directory()
+        self.image_handler_inst.move_image_to_work_title_directory()
         self.assertTrue(
-            Path(image_path_handler_temp_media_directory)
+            Path(image_handler_temp_media_directory)
             .joinpath("black_square_2019__/black_square.jpg")
             .exists()
         )
         self.assertFalse(self.image_file.exists())
 
     def test_can_not_move_image_to_non_existing_work_title_directory(self):
-        ImageHandler(self.image_file, "").move_image_to_work_title_directory()
+        ImageHandler(self.image_file).move_image_to_work_title_directory()
         self.assertTrue(self.image_file.exists())
 
-    def test_can_not_move_non_existing_image_to_work_title_directory(self):
-        ImageHandler(
-            self.non_existing_image_file, "No Title"
-        ).move_image_to_work_title_directory()
-        self.assertFalse(self.non_existing_image_file.exists())
-
     def test_can_delete_image(self):
-        self.image_path_handler_instance.delete_image()
+        self.image_handler_inst.delete_image()
         self.assertFalse(self.image_file.exists())
 
     def test_can_not_delete_image(self):
-        ImageHandler(self.non_existing_image_file, "").delete_image()
+        ImageHandler(self.non_existing_image_file).delete_image()
         self.assertFalse(self.non_existing_image_file.exists())
 
     def test_can_create_web_images_set_with_correct_sizes(self):
@@ -88,4 +91,7 @@ class ImageHandlerTest(TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        shutil.rmtree(image_path_handler_temp_media_directory)
+        import time
+
+        time.sleep(10)
+        shutil.rmtree(image_handler_temp_media_directory)
