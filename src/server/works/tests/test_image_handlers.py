e3d7843f7c8ade7930466b2e88bc67f6e1cf0530
taleldayekh <mail@talel.se>
2019-08-07 21:27:44 +0200

    Refactored ImageHandler to two different classes

--- src/server/works/tests/test_image_handlers.py
+++ src/server/works/tests/test_image_handlers.py
@@ -7,91 +7,135 @@ from django.test import TestCase, override_settings
 
 from PIL import Image
 
-from ..image_handlers import ImageHandler
+from ..image_handlers import ImageDirectoryHandler, ImageFileHandler
 
-image_handler_temp_media_directory = tempfile.mkdtemp(
-    prefix="image_path_handler_test_directory"
+image_directory_handler_test_folder = tempfile.mkdtemp(
+    prefix="image_directory_handler_test_folder"
+)
+image_file_handler_test_folder = tempfile.mkdtemp(
+    prefix="image_file_handler_test_folder"
 )
 
 
-@override_settings(MEDIA_ROOT=image_handler_temp_media_directory)
-class ImageHandlerTest(TestCase):
+@override_settings(MEDIA_ROOT=image_directory_handler_test_folder)
+class ImageDirectoryHandlerTest(TestCase):
+    def test_can_format_work_title(self):
+        image_directory_handler_inst_one = ImageDirectoryHandler(
+            "#bLAÄck SQuaRe, 20!.19;  "
+        )
+        image_directory_handler_inst_two = ImageDirectoryHandler(123)
+        self.assertEqual(
+            image_directory_handler_inst_one.formatted_work_title,
+            "black_square_2019__",
+        )
+        self.assertEqual(
+            image_directory_handler_inst_two.formatted_work_title, "123"
+        )
+
+    def test_can_create_directory_from_formatted_work_title(self):
+        image_directory = ImageDirectoryHandler("White Square")
+        image_directory.create_directory_from_formatted_work_title()
+        self.assertTrue(image_directory.image_directory_path.exists())
+
+    def test_can_not_create_directory_from_non_existing_work_title(self):
+        image_directory = ImageDirectoryHandler("")
+        image_directory.create_directory_from_formatted_work_title()
+        self.assertEqual(
+            image_directory.image_directory_path,
+            Path(image_directory_handler_test_folder),
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(image_directory_handler_test_folder)
+
+
+@override_settings(MEDIA_ROOT=image_file_handler_test_folder)
+class ImageFileHandlerTest(TestCase):
     def setUp(self):
-        self.image_file = Path(image_handler_temp_media_directory).joinpath(
+        self.image_file = Path(image_file_handler_test_folder).joinpath(
             "black_square.jpg"
         )
         test_image = Image.new("RGB", (500, 500))
         test_image.save(self.image_file)
         self.non_existing_image_file = Path("/path/to/non/existing/image")
-        self.image_handler_inst = ImageHandler(
-            self.image_file, "#bLAÄck SQuaRe, 20!.19;  "
-        )
 
-    def test_can_get_image_file_size(self):
-        image_file_size = self.image_handler_inst.image_file_size
-        self.assertEqual(image_file_size, 5)
+    def test_can_get_image_file_type(self):
+        image_file_type = ImageFileHandler(self.image_file).image_file_type
+        self.assertEqual(image_file_type, "JPEG")
 
-    def test_can_not_get_image_file_size(self):
-        image_file_size = ImageHandler(
-            self.non_existing_image_file, ""
-        ).image_file_size
-        self.assertEqual(image_file_size, None)
+    def test_can_not_get_image_file_type_from_non_existing_image(self):
+        with self.assertRaises(FileNotFoundError):
+            ImageFileHandler(self.non_existing_image_file).image_file_type
 
-    def test_can_format_work_title(self):
-        self.image_handler_inst._format_work_title()
-        self.assertEqual(
-            self.image_handler_inst.work_title_directory_name,
-            "black_square_2019__",
-        )
+    def test_can_not_get_image_file_type_from_non_image_file(self):
+        with tempfile.NamedTemporaryFile(
+            suffix=".pdf", dir=image_file_handler_test_folder
+        ) as pdf_file:
+            with self.assertRaises(OSError):
+                ImageFileHandler(Path(pdf_file.name)).image_file_type
 
-    def test_can_create_directory_from_work_title(self):
-        self.image_handler_inst._format_work_title()
-        self.image_handler_inst._create_directory_from_work_title()
-        self.assertTrue(
-            Path(self.image_handler_inst.work_title_directory_path).exists()
-        )
+    def test_can_get_floor_image_file_size(self):
+        image_file_size = ImageFileHandler(
+            self.image_file
+        ).floor_image_file_size
+        self.assertEqual(image_file_size, 4)
+
+    def test_can_not_get_floor_image_file_size_from_non_existing_image(self):
+        with self.assertRaises(FileNotFoundError):
+            ImageFileHandler(
+                self.non_existing_image_file
+            ).floor_image_file_size
 
     def test_can_move_image_to_work_title_directory(self):
-        self.image_handler_inst.move_image_to_work_title_directory()
+        image_directory = ImageDirectoryHandler("Black Square")
+        image_directory.create_directory_from_formatted_work_title()
+        image_directory_path = image_directory.image_directory_path
+
+        image_file_handler_inst = ImageFileHandler(
+            self.image_file, image_directory_path
+        )
+        image_file_handler_inst.move_image_to_work_title_directory()
         self.assertTrue(
-            Path(image_handler_temp_media_directory)
-            .joinpath("black_square_2019__/black_square.jpg")
-            .exists()
+            image_directory_path.joinpath(self.image_file.name).exists()
         )
         self.assertFalse(self.image_file.exists())
 
     def test_can_not_move_image_to_non_existing_work_title_directory(self):
-        ImageHandler(self.image_file).move_image_to_work_title_directory()
+        non_existing_work_title_directory = Path(
+            "path/to/non/existing/directory"
+        )
+
+        ImageFileHandler(
+            self.image_file, non_existing_work_title_directory
+        ).move_image_to_work_title_directory()
         self.assertTrue(self.image_file.exists())
 
     def test_can_delete_image(self):
-        self.image_handler_inst.delete_image()
+        image_file_handler_inst = ImageFileHandler(self.image_file)
+        image_file_handler_inst.delete_image()
         self.assertFalse(self.image_file.exists())
 
     def test_can_not_delete_image(self):
-        ImageHandler(self.non_existing_image_file).delete_image()
+        image_file_handler_inst = ImageFileHandler(
+            self.non_existing_image_file
+        )
+        image_file_handler_inst.delete_image()
         self.assertFalse(self.non_existing_image_file.exists())
 
     def test_can_create_web_images_set_with_correct_sizes(self):
-        web_image_set = ImageHandler.create_web_images_set(self.image_file)
+        web_images_set = ImageFileHandler.create_web_images_set(
+            self.image_file
+        )
         web_images = [
-            Image.open(web_image.image_file) for web_image in web_image_set
+            Image.open(web_image.image_file) for web_image in web_images_set
         ]
         self.assertEqual(web_images[0].size, (1250, 1250))
         self.assertEqual(web_images[1].size, (2500, 2500))
 
     def test_can_not_create_web_images_set_from_non_existing_image(self):
-        web_image_set = ImageHandler.create_web_images_set(
-            self.non_existing_image_file
-        )
-        self.assertEqual(web_image_set, None)
-
-    def test_can_create_unique_image_file_name(self):
         pass
 
     @classmethod
-    def tearDownClass(cls):
-        import time
-
-        time.sleep(10)
-        shutil.rmtree(image_handler_temp_media_directory)
+    def tearDownClass(self):
+        shutil.rmtree(image_file_handler_test_folder)
