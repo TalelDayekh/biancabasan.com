418f4ce9a110802c03de3a573a09e5db5b4b75d9
taleldayekh <mail@talel.se>
2019-08-13 08:06:39 +0200

    Renamed model test file

--- src/server/works/tests/test_models.py
+++ src/server/works/tests/test_models.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 
-from ..models import Work
+from ..models import Image, Work
 
 
-class WorkModelsTest(TestCase):
+class WorkModelTest(TestCase):
     def test_default_work(self):
         work = Work()
 
@@ -16,3 +16,19 @@ class WorkModelsTest(TestCase):
         self.assertEqual(work.depth, None)
         self.assertEqual(work.description, "")
         self.assertEqual(work.date_added, None)
+
+
+class ImageModelTest(TestCase):
+    def test_image_is_related_to_work(self):
+        work = Work(
+            title="Black Square",
+            year_to=2019,
+            technique="Oil on canvas",
+            description="A painting of a black square",
+        )
+        work.save()
+        image = Image()
+        image.work = work
+        image.save()
+
+        self.assertIn(image, work.images.all())
