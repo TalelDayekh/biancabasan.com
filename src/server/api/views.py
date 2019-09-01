99fe909a19f51e01d1eaf2aacadc58109fa1d57a
taleldayekh <mail@talel.se>
2019-09-01 13:02:24 +0200

    Works GET request

--- src/server/api/views.py
+++ src/server/api/views.py
@@ -1,4 +1,10 @@
+from django.http import HttpRequest
+
 from api.v1.serializers import ImageSerializerVersion1, WorkSerializerVersion1
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from works.models import Work
 
 
 class GetSerializerClasses:
@@ -15,3 +21,13 @@ class GetSerializerClasses:
     @property
     def image_serializer(self) -> object:
         return self.api_image_serializer_mapping.get(self.api_version)
+
+
+class Works(APIView):
+    def get(
+        self, request: HttpRequest, version: str, username: str, format=None
+    ) -> Response:
+        work_serializer = GetSerializerClasses(version).work_serializer
+        works = Work.objects.filter(owner__username=username)
+        serializer = work_serializer(works, many=True)
+        return Response(serializer.data)
