db731f5f19931615ecbe0d8a23771b54f5f88e8e
taleldayekh <mail@talel.se>
2019-09-01 11:20:08 +0200

    Getting serializers based on API version and test

--- src/server/api/views.py
+++ src/server/api/views.py
@@ -1,1 +1,17 @@
-from rest_framework.decorators import api_view
+from api.v1.serializers import ImageSerializerVersion1, WorkSerializerVersion1
+
+
+class GetSerializerClasses:
+    api_work_serializer_mapping = {"v1": WorkSerializerVersion1}
+    api_image_serializer_mapping = {"v1": ImageSerializerVersion1}
+
+    def __init__(self, api_version: str) -> None:
+        self.api_version = api_version
+
+    @property
+    def work_serializer(self) -> object:
+        return self.api_work_serializer_mapping.get(self.api_version)
+
+    @property
+    def image_serializer(self) -> object:
+        return self.api_image_serializer_mapping.get(self.api_version)
