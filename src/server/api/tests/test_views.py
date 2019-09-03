bf8472e4d0c0746ac35a9a2049f22902efcc8b89
taleldayekh <mail@talel.se>
2019-09-03 07:10:07 +0200

    Works GET request test

--- src/server/api/tests/test_views.py
+++ src/server/api/tests/test_views.py
@@ -1,6 +1,13 @@
-from django.test import TestCase
+import json
 
-from api.views import GetSerializerClasses
+from django.test import TestCase
+from django.urls import reverse
+
+from api.views import GetSerializerClasses, Works
+from rest_framework.test import APITestCase
+from users.models import CustomUser
+
+from works.models import Work
 
 
 class SerializerRetrievalTest(TestCase):
@@ -10,3 +17,28 @@ class SerializerRetrievalTest(TestCase):
 
         self.assertEqual(work_serializer.__name__, "WorkSerializerVersion1")
         self.assertEqual(image_serializer.__name__, "ImageSerializerVersion1")
+
+
+class WorksAPIRequestTest(APITestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create(username="matisse")
+        self.work = Work.objects.create(
+            owner=self.user,
+            title="Dance",
+            year_from=1910,
+            year_to=1910,
+            technique="Oil on canvas",
+            height=260,
+            width=391,
+            description=(
+                "The painting shows five dancing figures, painted in a strong red,"
+                " set against a very simplified green landscape and deep blue sky."
+            ),
+        )
+
+    def test_can_get_works_for_specific_user(self):
+        response = self.client.get(
+            reverse("Works", kwargs={"version": "v1", "username": "matisse"})
+        )
+
+        self.assertEqual(response.status_code, 200)
