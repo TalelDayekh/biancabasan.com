4d697dd25bf12df26f0b9e630dd32638df2017fc
taleldayekh <mail@talel.se>
2019-09-03 19:41:24 +0200

    Initial commit

---
+++ src/server/api/v1/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from users.models import CustomUser
+
+from works.models import Image, Work
+
+
+class OwnerSerializerVersion1(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "id"]
+
+
+class ImageSerializerVersion1(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ["id", "image"]
+
+
+class WorkSerializerVersion1(serializers.ModelSerializer):
+    owner = OwnerSerializerVersion1(read_only=True)
+    images = ImageSerializerVersion1(many=True, read_only=True)
+
+    class Meta:
+        model = Work
+        fields = [
+            "owner",
+            "id",
+            "title",
+            "year_from",
+            "year_to",
+            "technique",
+            "height",
+            "width",
+            "depth",
+            "description",
+            "created_at",
+            "images",
+        ]
