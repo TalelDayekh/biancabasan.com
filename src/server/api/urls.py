dee1dc57dc1d4e556f5eee7db3e2172bf1ecca32
taleldayekh <mail@talel.se>
2019-09-03 19:42:21 +0200

    Added name argument

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path("<str:version>/users/<str:username>/works/", views.Works.as_view())
+    path(
+        "<str:version>/users/<str:username>/works/",
+        views.Works.as_view(),
+        name="Works",
+    )
 ]
