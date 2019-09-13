cbd6110c9c90a741867eb2b7b57c2d8d974b0574
taleldayekh <mail@talel.se>
2019-09-13 07:20:22 +0200

    Name change and url update

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -4,8 +4,8 @@ from api import views
 
 urlpatterns = [
     path(
-        "<str:version>/users/<str:username>/works/",
-        views.Works.as_view(),
+        "<str:version>/users/<str:username>/works",
+        views.AllWorks.as_view(),
         name="Works",
     )
 ]
