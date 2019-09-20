847d0c866f83d749cb910e35415e8959b987ec4a
taleldayekh <mail@talel.se>
2019-09-20 23:14:40 +0200

    Removed username from URL path

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -3,13 +3,9 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
+    path("<str:version>/works/", views.AllWorks.as_view(), name="all-works"),
     path(
-        "<str:version>/users/<str:username>/works/",
-        views.AllWorks.as_view(),
-        name="all-works",
-    ),
-    path(
-        "<str:version>/users/<str:username>/works/<int:pk>/",
+        "<str:version>/works/<int:pk>/",
         views.SingleWork.as_view(),
         name="single-work",
     ),
