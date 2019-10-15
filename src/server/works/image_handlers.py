fd71ff57f35bb1aa485bf643c9fdb408c62809a5
taleldayekh <mail@talel.se>
2019-10-15 21:38:41 +0200

    Refactoring of image handlers and tests

--- src/server/works/image_handlers.py
+++ src/server/works/image_handlers.py
@@ -4,132 +4,95 @@ import os
 import re
 import shutil
 import uuid
-from math import floor
 from pathlib import Path
-from typing import Optional
+from typing import Type
 
-from django.conf import settings
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from PIL import Image
 
 
-class ImageDirectoryHandler:
-    def __init__(self, work_title: str) -> None:
-        self.work_title = str(work_title)
-        self.formatted_work_title = self._format_work_title()
-        self.image_directory_path = Path(settings.MEDIA_ROOT).joinpath(
-            self.formatted_work_title
-        )
+def image_upload_handler(instance: Type[ImageModel], filename: str) -> str:
+    uid = uuid.uuid4().hex
+    image_name = Path(filename).stem
+    image_type = Path(filename).suffix
+    image_file = image_name + "_" + uid + image_type
 
-    def _format_work_title(self) -> str:
-        # Removes characters which are not letters or numbers
-        # from a work title and replaces all whitespaces with
-        # underscores and converts the string into lowercase.
-        formatted_work_title = (
-            re.sub("[^A-Za-z0-9\s]{1}", "", self.work_title)
-            .replace(" ", "_")
-            .lower()
-        )
-        return formatted_work_title
+    formatted_work_title = format_work_title(instance.work)
+    image_directory = "images/" + formatted_work_title + "/"
 
-    def create_directory_from_formatted_work_title(self) -> None:
-        try:
-            if not self.work_title:
-                raise ValueError(
-                    "A work title has to be provided for creating a directory"
-                )
-            else:
-                work_title_directory = self.image_directory_path
-                work_title_directory.mkdir(parents=True, exist_ok=True)
-        except Exception as err:
-            print(err)
+    upload_to = image_directory + image_file
+    return upload_to
+
+
+def format_work_title(work_title: str) -> str:
+    # Removes characters which are not letters or numbers
+    # from a work title and replaces all whitespaces with
+    # underscores and converts the string into lowercase.
+    formatted_work_title = (
+        re.sub("[^A-Za-z0-9\s]{1}", "", str(work_title))
+        .replace(" ", "_")
+        .lower()
+    )
+    return formatted_work_title
 
 
 class ImageValidationHandler:
-    maximum_image_size = 2097152
-    allowed_image_format = ["JPEG"]
-
     def __init__(self, image_file: InMemoryUploadedFile) -> None:
         self.image_file = image_file
 
     def _validate_image_format(self) -> bool:
+        allowed_image_format = ["JPEG"]
+
         try:
             image = Image.open(self.image_file)
-            return True if image.format in self.allowed_image_format else False
+            return True if image.format in allowed_image_format else False
         except IOError:
             return False
 
     def _validate_image_size(self) -> bool:
-        return (
-            True if self.image_file.size <= self.maximum_image_size else False
-        )
+        maximum_image_size = 2097152
+        return True if self.image_file.size <= maximum_image_size else False
 
     def is_valid(self) -> bool:
         return self._validate_image_format() and self._validate_image_size()
 
 
 class ImageFileHandler:
-    image_widths = [1250, 2500]
-    image_quality = 20
-
-    def __init__(
-        self, image_file: Path, work_title_directory: Optional[Path] = None
-    ) -> None:
+    def __init__(self, image_file: Path) -> None:
         self.image_file = image_file
-        self.work_title_directory = work_title_directory
 
-    @property
-    def image_file_type(self) -> str:
-        return Image.open(self.image_file).format
+    def delete_image_set(self):
+        image_file_directory = self.image_file.parent
+        image_name = self.image_file.stem
+        images = os.listdir(image_file_directory)
 
-    @property
-    def floor_image_file_size(self) -> int:
-        # Returns size in byte
-        return floor(self.image_file.stat().st_size / 1024)
-
-    def move_image_to_work_title_directory(self) -> None:
-        try:
-            if (
-                not self.image_file.exists()
-                or not self.work_title_directory.exists()
-            ):
-                raise Exception(
-                    "A valid image file path and work title directory path has to be provided"
-                )
-            else:
-                shutil.move(
-                    str(self.image_file), str(self.work_title_directory)
-                )
-        except Exception as err:
-            print(err)
-
-    def delete_image(self) -> None:
-        try:
-            os.remove(self.image_file)
-        except OSError as err:
-            print("No image file to delete")
+        for image in images:
+            if image_name in image:
+                os.remove(image_file_directory.joinpath(image))
 
     @classmethod
-    def create_web_images_set(
-        cls, image_file: Path, work_title_directory: Optional[Path] = None
-    ) -> List[ImageFileHandler]:
+    def create_web_image_set(
+        cls, image_file: Path
+    ) -> List[Type[ImageFileHandler]]:
+        image_widths = [1250, 2500]
+        image_quality = 20
+
         try:
             Image.open(image_file)
         except OSError as err:
             print("A valid image file path has to be provided")
         else:
-            uid = str(uuid.uuid4().hex)
-            web_images = [
+            web_image_files = [
                 image_file.parent.joinpath(
-                    "_".join([str(image_file.stem), uid, str(image_width)])
-                    + str(image_file.suffix)
+                    "_".join([image_file.stem, str(image_width)])
+                    + image_file.suffix
                 )
-                for image_width in cls.image_widths
+                for image_width in image_widths
             ]
 
             for web_image_file, image_width in zip(
-                web_images, cls.image_widths
+                web_image_files, image_widths
             ):
                 shutil.copyfile(image_file, web_image_file)
 
@@ -143,12 +106,6 @@ class ImageFileHandler:
                 resized_image_file = Image.open(web_image_file).resize(
                     (image_width, int(round(resized_image_height)))
                 )
-                resized_image_file.save(
-                    web_image_file, quality=cls.image_quality
-                )
 
-            os.remove(image_file)
-            return [
-                cls(web_image, work_title_directory)
-                for web_image in web_images
-            ]
+                resized_image_file.save(web_image_file, quality=image_quality)
+            return [cls(web_image) for web_image in web_image_files]
--- src/server/works/tests/test_image_handlers.py
+++ src/server/works/tests/test_image_handlers.py
@@ -4,58 +4,33 @@ from contextlib import contextmanager
 from pathlib import Path
 
 from django.core.files.uploadedfile import InMemoryUploadedFile
-from django.test import TestCase, override_settings
+from django.test import TestCase
 
 from PIL import Image
 
-from ..image_handlers import (ImageDirectoryHandler, ImageFileHandler,
-                              ImageValidationHandler)
-
-image_directory_handler_test_folder = tempfile.mkdtemp(
-    prefix="image_directory_handler_test_folder"
+from ..image_handlers import (
+    ImageFileHandler,
+    ImageValidationHandler,
+    format_work_title,
 )
+
 image_file_handler_test_folder = tempfile.mkdtemp(
     prefix="image_file_handler_test_folder"
 )
 
 
-@override_settings(MEDIA_ROOT=image_directory_handler_test_folder)
-class ImageDirectoryHandlerTest(TestCase):
-    def setUp(self):
-        self.image_directory_handler = ImageDirectoryHandler(
-            "#bLAÄck SQuaRe, 20!.19;  "
-        )
+class FormatWorkTitleTest(TestCase):
+    def test_can_format_work_title_str(self):
+        work_title = "#bLAÄck SQuaRe, 20!.19;  "
+        formatted_work_title = format_work_title(work_title)
 
-    def test_can_format_work_title(self):
-        formatted_work_title_one = (
-            self.image_directory_handler.formatted_work_title
-        )
-        formatted_work_title_two = ImageDirectoryHandler(
-            123
-        ).formatted_work_title
+        self.assertEqual(formatted_work_title, "black_square_2019__")
 
-        self.assertEqual(formatted_work_title_one, "black_square_2019__")
-        self.assertEqual(formatted_work_title_two, "123")
+    def test_can_format_work_title_int(self):
+        work_title = 123
+        formatted_work_title = format_work_title(work_title)
 
-    def test_can_create_directory_from_formatted_work_title(self):
-        self.image_directory_handler.create_directory_from_formatted_work_title()
-
-        self.assertTrue(
-            self.image_directory_handler.image_directory_path.exists()
-        )
-
-    def test_cannot_create_directory_from_non_existing_work_title(self):
-        image_directory = ImageDirectoryHandler("")
-        image_directory.create_directory_from_formatted_work_title()
-
-        self.assertEqual(
-            image_directory.image_directory_path,
-            Path(image_directory_handler_test_folder),
-        )
-
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(image_directory_handler_test_folder)
+        self.assertEqual(formatted_work_title, "123")
 
 
 class ImageValidationHandlerTest(TestCase):
@@ -83,168 +58,104 @@ class ImageValidationHandlerTest(TestCase):
         finally:
             temporary_image_file.close()
 
+    def image_format_validator(self, image_format: str) -> bool:
+        with self.create_temporary_in_memory_image_file(
+            image_format, 2097152
+        ) as temporary_in_memory_image:
+            valid_image_format = ImageValidationHandler(
+                temporary_in_memory_image
+            )._validate_image_format()
+            return valid_image_format
+
+    def image_size_validator(self, image_size: int) -> bool:
+        with self.create_temporary_in_memory_image_file(
+            "JPEG", image_size
+        ) as temporary_in_memory_image:
+            valid_image_size = ImageValidationHandler(
+                temporary_in_memory_image
+            )._validate_image_size()
+            return valid_image_size
+
+    def image_validator(self, image_format: str, image_size: int) -> bool:
+        with self.create_temporary_in_memory_image_file(
+            image_format, image_size
+        ) as temporary_in_memory_image:
+            valid_image = ImageValidationHandler(
+                temporary_in_memory_image
+            ).is_valid()
+            return valid_image
+
     def test_can_validate_image_format_as_true_or_false(self):
-        with self.create_temporary_in_memory_image_file(
-            "JPEG", 2097152
-        ) as temporary_in_memory_image:
-            valid_image_format = ImageValidationHandler(
-                temporary_in_memory_image
-            )._validate_image_format()
+        valid_image_format = self.image_format_validator("JPEG")
+        invalid_image_format = self.image_format_validator("PNG")
 
-            self.assertEqual(valid_image_format, True)
-
-        with self.create_temporary_in_memory_image_file(
-            "PNG", 2097152
-        ) as temporary_in_memory_image:
-            valid_image_format = ImageValidationHandler(
-                temporary_in_memory_image
-            )._validate_image_format()
-
-            self.assertEqual(valid_image_format, False)
+        self.assertEqual(valid_image_format, True)
+        self.assertEqual(invalid_image_format, False)
 
     def test_can_validate_image_size_as_true_or_false(self):
-        with self.create_temporary_in_memory_image_file(
-            "JPEG", 2097152
-        ) as temporary_in_memory_image:
-            valid_image_size = ImageValidationHandler(
-                temporary_in_memory_image
-            )._validate_image_size()
+        valid_image_size = self.image_size_validator(2097152)
+        invalid_image_size = self.image_size_validator(2097153)
 
-            self.assertEqual(valid_image_size, True)
-
-        with self.create_temporary_in_memory_image_file(
-            "JPEG", 2097153
-        ) as temporary_in_memory_image:
-            valid_image_size = ImageValidationHandler(
-                temporary_in_memory_image
-            )._validate_image_size()
-
-            self.assertEqual(valid_image_size, False)
+        self.assertEqual(valid_image_size, True)
+        self.assertEqual(invalid_image_size, False)
 
     def test_can_validate_image_as_true_or_false(self):
-        with self.create_temporary_in_memory_image_file(
-            "JPEG", 2097152
-        ) as temporary_in_memory_image:
-            valid_image = ImageValidationHandler(
-                temporary_in_memory_image
-            ).is_valid()
+        invalid_image_one = self.image_validator("PNG", 2097152)
+        invalid_image_two = self.image_validator("JPEG", 2097153)
+        valid_image = self.image_validator("JPEG", 2097152)
 
-            self.assertEqual(valid_image, True)
-
-        with self.create_temporary_in_memory_image_file(
-            "PNG", 2097152
-        ) as temporary_in_memory_image:
-            valid_image = ImageValidationHandler(
-                temporary_in_memory_image
-            ).is_valid()
-
-            self.assertEqual(valid_image, False)
-
-        with self.create_temporary_in_memory_image_file(
-            "JPEG", 2097153
-        ) as temporary_in_memory_image:
-            valid_image = ImageValidationHandler(
-                temporary_in_memory_image
-            ).is_valid()
-
-            self.assertEqual(valid_image, False)
+        self.assertEqual(invalid_image_one, False)
+        self.assertEqual(invalid_image_two, False)
+        self.assertEqual(valid_image, True)
 
 
-@override_settings(MEDIA_ROOT=image_file_handler_test_folder)
 class ImageFileHandlerTest(TestCase):
     def setUp(self):
+        self.nonexistent_image_file = Path("/path/to/nonexistent/image")
         self.image_file = Path(image_file_handler_test_folder).joinpath(
             "black_square.jpg"
         )
         test_image = Image.new("RGB", (500, 500))
         test_image.save(self.image_file)
-        self.non_existing_image_file = Path("/path/to/non/existing/image")
-        self.image_file_handler_valid_image = ImageFileHandler(self.image_file)
-        self.image_file_handler_invalid_image = ImageFileHandler(
-            self.non_existing_image_file
-        )
 
-    def test_can_get_image_file_type(self):
-        image_file_type = self.image_file_handler_valid_image.image_file_type
-
-        self.assertEqual(image_file_type, "JPEG")
-
-    def test_cannot_get_image_file_type_from_non_existing_image(self):
-        with self.assertRaises(FileNotFoundError):
-            self.image_file_handler_invalid_image.image_file_type
-
-    def test_cannot_get_image_file_type_from_non_image_file(self):
-        with tempfile.NamedTemporaryFile(
-            suffix=".pdf", dir=image_file_handler_test_folder
-        ) as pdf_file:
-            with self.assertRaises(OSError):
-                ImageFileHandler(Path(pdf_file.name)).image_file_type
-
-    def test_can_get_floor_image_file_size(self):
-        image_file_size = (
-            self.image_file_handler_valid_image.floor_image_file_size
-        )
-
-        self.assertEqual(image_file_size, 4)
-
-    def test_cannot_get_floor_image_file_size_from_non_existing_image(self):
-        with self.assertRaises(FileNotFoundError):
-            self.image_file_handler_invalid_image.floor_image_file_size
-
-    def test_can_move_image_to_work_title_directory(self):
-        image_directory_handler = ImageDirectoryHandler("Black Square")
-        image_directory_handler.create_directory_from_formatted_work_title()
-        image_directory_path = image_directory_handler.image_directory_path
-
-        image_file_handler = ImageFileHandler(
-            self.image_file, image_directory_path
-        )
-        image_file_handler.move_image_to_work_title_directory()
-
-        self.assertTrue(
-            image_directory_path.joinpath(self.image_file.name).exists()
-        )
-        self.assertFalse(self.image_file.exists())
-
-    def test_cannot_move_image_to_non_existing_work_title_directory(self):
-        non_existing_work_title_directory = Path(
-            "path/to/non/existing/directory"
-        )
-
-        ImageFileHandler(
-            self.image_file, non_existing_work_title_directory
-        ).move_image_to_work_title_directory()
-
-        self.assertTrue(self.image_file.exists())
-
-    def test_can_delete_image(self):
-        self.image_file_handler_valid_image.delete_image()
-
-        self.assertFalse(self.image_file.exists())
-
-    def test_cannot_delete_image(self):
-        self.image_file_handler_invalid_image.delete_image()
-
-        self.assertFalse(self.non_existing_image_file.exists())
-
-    def test_can_create_web_images_set_with_correct_sizes(self):
-        web_images_set = ImageFileHandler.create_web_images_set(
+        self.web_image_set = ImageFileHandler.create_web_image_set(
             self.image_file
         )
+
+    def test_can_create_web_image_set_with_correct_sizes(self):
         web_images = [
-            Image.open(web_image.image_file) for web_image in web_images_set
+            Image.open(web_image.image_file)
+            for web_image in self.web_image_set
         ]
 
         self.assertEqual(web_images[0].size, (1250, 1250))
         self.assertEqual(web_images[1].size, (2500, 2500))
 
-    def test_cannot_create_web_images_set_from_non_existing_image(self):
-        web_images_set = ImageFileHandler.create_web_images_set(
-            self.non_existing_image_file
+    def test_cannot_create_web_image_set_from_nonexistent_image(self):
+        web_image_set = ImageFileHandler.create_web_image_set(
+            self.nonexistent_image_file
         )
 
-        self.assertEqual(web_images_set, None)
+        self.assertEqual(web_image_set, None)
+
+    def test_can_delete_image_set(self):
+        image_file_1250 = Path(image_file_handler_test_folder).joinpath(
+            "black_square_1250.jpg"
+        )
+        image_file_2500 = Path(image_file_handler_test_folder).joinpath(
+            "black_square_2500.jpg"
+        )
+
+        ImageFileHandler(self.image_file).delete_image_set()
+
+        self.assertEqual(self.image_file.exists(), False)
+        self.assertEqual(image_file_1250.exists(), False)
+        self.assertEqual(image_file_2500.exists(), False)
+
+    def test_cannot_delete_nonexistent_image_file(self):
+        with self.assertRaises(FileNotFoundError):
+            ImageFileHandler(self.nonexistent_image_file).delete_image_set()
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         shutil.rmtree(image_file_handler_test_folder)
