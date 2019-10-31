493589397b1770d422a2eecdaa749846cbe9ab72
taleldayekh <mail@talel.se>
2019-10-31 22:47:39 +0100

    Slight refactoring of works API

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -4,6 +4,9 @@ from api import views
 
 urlpatterns = [
     url(
+        r"^(?P<version>\w+)/works$", views.WorkList.as_view(), name="work_list"
+    ),
+    url(
         r"^(?P<version>\w+)/works/(?P<work_id>\d+)/images$",
         views.ImageList.as_view(),
         name="image_list",
@@ -14,60 +17,3 @@ urlpatterns = [
         name="image_detail",
     ),
 ]
-
-
-# delete a specific image for a specific work
-
-# urlpatterns = [
-#     url(r'^(?P<version>.*)/(?P<username>[/.*])/images', views.AllImages.as_view(), name='all_images')
-#     # url(r'^(?P<version>(.+:?))/works$', views.AllImages.as_view(), name='all_images')
-# ]
-
-
-# from django.urls import path, register_converter
-# from django.urls.converters import StringConverter
-
-# from api import views
-
-
-# class BlaConverter:
-#     regex = '.*'
-
-#     def to_python(self, value):
-#         if value:
-#             print(value)
-#             return value
-#         else:
-#             return None
-
-#     def to_url(self, value):
-#         return value if value is not None else ''
-
-# register_converter(BlaConverter, 'whatever')
-
-
-# urlpatterns = [
-#     path("<str:version>/works/", views.AllWorks.as_view(), name="all-works"),
-#     path(
-#         "<str:version>/works/<int:work_id>/",
-#         views.SingleWork.as_view(),
-#         name="single-work",
-#     ),
-#     path(
-#         "<str:version>/<str:username>/works/years/",
-#         views.YearsToList.as_view(),
-#         name="years-to",
-#     ),
-
-
-#     path(
-#         "<str:version>/users/<whatever:username>/",
-#         views.AllImages.as_view(),
-#         name="all-images",
-#     ),
-#     # path(
-#     #     "<str:version>/users/<str:username>/works/<int:work_id>/images/<int:image_id>/",
-#     #     views.SingleImage.as_view(),
-#     #     name="single-image",
-#     # ),
-# ]
