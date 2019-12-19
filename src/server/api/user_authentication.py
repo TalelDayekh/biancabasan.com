a8f2b0417f94cc56cbad7a65fa48e4f5b10c57c5
taleldayekh <mail@talel.se>
2019-12-19 21:39:47 +0100

    Password strength validator and test

--- src/server/api/user_authentication.py
+++ src/server/api/user_authentication.py
@@ -1,5 +1,28 @@
+from django.http import HttpRequest
+
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+def password_strength_validator(password: str) -> bool:
+    # Checks that a password has at least one capitalized letter,
+    # one number and is longer than or equal to eight characters.
+    password = str(password)
+    return (
+        any(character.isupper() for character in password)
+        and any(character.isdigit() for character in password)
+        and len(password) >= 8
+    )
 
 
 class AuthenticationToken(ObtainAuthToken):
     pass
+
+
+class PasswordChange(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def patch(self, request: HttpRequest, version: str) -> Response:
+        pass
