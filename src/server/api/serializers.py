7f7ddb5c01486c0e85ad974915dc8d9a9251cc74
taleldayekh <mail@talel.se>
2019-12-20 23:46:35 +0100

    Moving AuthenticationSerializer to separate file

---
+++ src/server/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+
+class AuthenticationSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    new_password_confirm = serializers.CharField(required=True)
