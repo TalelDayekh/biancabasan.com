cefb29da50f36b8233a4365b50e494c998092126
taleldayekh <mail@talel.se>
2019-12-23 17:19:15 +0100

    Adding password request email serializer

--- src/server/api/serializers.py
+++ src/server/api/serializers.py
@@ -5,3 +5,7 @@ class AuthenticationSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
     new_password_confirm = serializers.CharField(required=True)
+
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
