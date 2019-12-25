bfb4408da898cc8ac41e5edb2a75a83e17588886
taleldayekh <mail@talel.se>
2019-12-25 22:18:11 +0100

    Generating the password reset url

--- src/server/api/user_authentication.py
+++ src/server/api/user_authentication.py
@@ -1,4 +1,11 @@
+from pathlib import Path
+
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.http import HttpRequest
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from api.exceptions import ValidationError
 from api.serializers import (
@@ -65,17 +72,30 @@ class PasswordChange(APIView):
 
 
 class PasswordReset(APIView):
-    def _generate_password_reset_email(self, email: str) -> None:
+    def _send_password_reset_email(
+        self, email: str, domain: str, use_https: bool = False
+    ) -> None:
         try:
             user = CustomUser.objects.get(email=email)
-        except Exception as err:
-            print(err)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            token = default_token_generator.make_token(user)
+            protocol = "https://" if use_https else "http://"
+            password_reset_url = Path(protocol).joinpath(
+                domain, "password_reset", uid, token
+            )
+        except Exception:
+            raise ValidationError("Email does not exist.")
 
     def post(self, request: HttpRequest, version: str) -> Response:
         serializer = PasswordResetRequestSerializer(data=request.data)
 
         if serializer.is_valid():
             email = serializer.data["email"]
-            self._generate_password_reset_email(email)
-            return Response(status=status.HTTP_200_OK)
+            domain = get_current_site(request).domain
+
+            try:
+                self._send_password_reset_email(email, domain)
+                return Response(status=status.HTTP_200_OK)
+            except ValidationError as err:
+                return Response(str(err), status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
