fe542237676b90fcd864ff8d2252b96efc4b31c4
taleldayekh <mail@talel.se>
2019-12-28 14:00:36 +0100

    Password reset serializer and slight refactor

--- src/server/api/serializers.py
+++ src/server/api/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
 
-class AuthenticationSerializer(serializers.Serializer):
-    old_password = serializers.CharField(required=True)
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+
+class PasswordResetSerializer(serializers.Serializer):
     new_password = serializers.CharField(required=True)
     new_password_confirm = serializers.CharField(required=True)
 
 
-class PasswordResetRequestSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True)
+class PasswordUpdateSerializer(PasswordResetSerializer):
+    old_password = serializers.CharField(required=True)
