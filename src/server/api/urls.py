4143823bf71d64dd856105cac6b047feaf8f9a31
taleldayekh <mail@talel.se>
2020-01-05 14:24:14 +0100

    Correcting request in test and disabling old URLs

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -1,49 +1,49 @@
 from django.conf.urls import url
 
-from api import user_authentication, views
+# from api import user_authentication, views
 
 urlpatterns = [
-    url(
-        r"^(?P<version>\w+)/login$",
-        user_authentication.AuthenticationToken.as_view(),
-        name="authentication_token",
-    ),
-    url(
-        r"^(?P<version>\w+)/password$",
-        user_authentication.PasswordUpdate.as_view(),
-        name="password_update",
-    ),
-    url(
-        r"^(?P<version>\w+)/password_reset$",
-        user_authentication.PasswordReset.as_view(),
-        name="password_reset_request",
-    ),
-    url(
-        r"^(?P<version>\w+)/password_reset/(?P<uid>\S+)/(?P<token>\S+)$",
-        user_authentication.PasswordReset.as_view(),
-        name="password_reset",
-    ),
-    url(
-        r"^(?P<version>\w+)/works$", views.WorkList.as_view(), name="work_list"
-    ),
-    url(
-        r"^(?P<version>\w+)/works/(?P<work_id>\d+)$",
-        views.WorkDetail.as_view(),
-        name="work_detail",
-    ),
-    url(
-        r"^(?P<version>\w+)/works/years$",
-        views.WorkYearsList.as_view(),
-        name="work_years_list",
-    ),
-    url(
-        r"^(?P<version>\w+)/works/(?P<work_id>\d+)/images$",
-        views.ImageList.as_view(),
-        name="image_list",
-    ),
-    url(
-        r"^(?P<version>\w+)/works/(?P<work_id>\d+)/images/(?P<image_id>\d+)$",
-        views.ImageDetail.as_view(),
-        name="image_detail",
-    ),
+    # url(
+    #     r"^(?P<version>\w+)/login$",
+    #     user_authentication.AuthenticationToken.as_view(),
+    #     name="authentication_token",
+    # ),
+    # url(
+    #     r"^(?P<version>\w+)/password$",
+    #     user_authentication.PasswordUpdate.as_view(),
+    #     name="password_update",
+    # ),
+    # url(
+    #     r"^(?P<version>\w+)/password_reset$",
+    #     user_authentication.PasswordReset.as_view(),
+    #     name="password_reset_request",
+    # ),
+    # url(
+    #     r"^(?P<version>\w+)/password_reset/(?P<uid>\S+)/(?P<token>\S+)$",
+    #     user_authentication.PasswordReset.as_view(),
+    #     name="password_reset",
+    # ),
+    # url(
+    #     r"^(?P<version>\w+)/works$", views.WorkList.as_view(), name="work_list"
+    # ),
+    # url(
+    #     r"^(?P<version>\w+)/works/(?P<work_id>\d+)$",
+    #     views.WorkDetail.as_view(),
+    #     name="work_detail",
+    # ),
+    # url(
+    #     r"^(?P<version>\w+)/works/years$",
+    #     views.WorkYearsList.as_view(),
+    #     name="work_years_list",
+    # ),
+    # url(
+    #     r"^(?P<version>\w+)/works/(?P<work_id>\d+)/images$",
+    #     views.ImageList.as_view(),
+    #     name="image_list",
+    # ),
+    # url(
+    #     r"^(?P<version>\w+)/works/(?P<work_id>\d+)/images/(?P<image_id>\d+)$",
+    #     views.ImageDetail.as_view(),
+    #     name="image_detail",
+    # ),
 ]
