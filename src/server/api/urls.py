a2ec8fdafe2713e5129806ad7162a74d5d10ba6b
taleldayekh <mail@talel.se>
2020-01-06 11:50:37 +0100

    Refactoring URL for work resource

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -3,29 +3,7 @@ from django.conf.urls import url
 # from api import user_authentication, views
 
 urlpatterns = [
-    # url(
-    #     r"^(?P<version>\w+)/login$",
-    #     user_authentication.AuthenticationToken.as_view(),
-    #     name="authentication_token",
-    # ),
-    # url(
-    #     r"^(?P<version>\w+)/password$",
-    #     user_authentication.PasswordUpdate.as_view(),
-    #     name="password_update",
-    # ),
-    # url(
-    #     r"^(?P<version>\w+)/password_reset$",
-    #     user_authentication.PasswordReset.as_view(),
-    #     name="password_reset_request",
-    # ),
-    # url(
-    #     r"^(?P<version>\w+)/password_reset/(?P<uid>\S+)/(?P<token>\S+)$",
-    #     user_authentication.PasswordReset.as_view(),
-    #     name="password_reset",
-    # ),
-    # url(
-    #     r"^(?P<version>\w+)/works$", views.WorkList.as_view(), name="work_list"
-    # ),
+
     # url(
     #     r"^(?P<version>\w+)/works/(?P<work_id>\d+)$",
     #     views.WorkDetail.as_view(),
--- src/server/api/v2/urls.py
+++ src/server/api/v2/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from . import auth
+from . import auth, work
 
 urlpatterns = [
     url(r"^auth/login$", auth.Login.as_view(), name="login"),
@@ -20,4 +20,5 @@ urlpatterns = [
         auth.PasswordReset.as_view(),
         name="password_reset",
     ),
+    url(r"^/works$", work.WorkList.as_view(), name="work_list"),
 ]
