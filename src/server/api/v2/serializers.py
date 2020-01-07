0bcc247183433e083ff20043641e43252e524fd0
taleldayekh <mail@talel.se>
2020-01-07 17:13:14 +0100

    Refactoring serializers for API v1

--- src/server/api/v2/serializers.py
+++ src/server/api/v2/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from users.models import CustomUser
+
+from works.models import Image, Work
 
 
 class PasswordResetEmailSerializer(serializers.Serializer):
@@ -12,3 +15,37 @@ class PasswordResetSerializer(serializers.Serializer):
 
 class PasswordUpdateSerializer(PasswordResetSerializer):
     old_password = serializers.CharField(required=True)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "id"]
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ["work_id", "id", "image"]
+
+
+class WorkSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    images = ImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Work
+        fields = [
+            "user",
+            "id",
+            "title",
+            "year_from",
+            "year_to",
+            "technique",
+            "height",
+            "width",
+            "depth",
+            "description",
+            "created_at",
+            "images",
+        ]
