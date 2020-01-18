e99f0126f02d06a7db9a8e4958c4c95deea7cdb7
taleldayekh <mail@talel.se>
2020-01-18 22:17:32 +0100

    Disabled files while refactoring

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -3,17 +3,6 @@ from django.conf.urls import url
 # from api import user_authentication, views
 
 urlpatterns = [
-
-    # url(
-    #     r"^(?P<version>\w+)/works/(?P<work_id>\d+)$",
-    #     views.WorkDetail.as_view(),
-    #     name="work_detail",
-    # ),
-    # url(
-    #     r"^(?P<version>\w+)/works/years$",
-    #     views.WorkYearsList.as_view(),
-    #     name="work_years_list",
-    # ),
     # url(
     #     r"^(?P<version>\w+)/works/(?P<work_id>\d+)/images$",
     #     views.ImageList.as_view(),
