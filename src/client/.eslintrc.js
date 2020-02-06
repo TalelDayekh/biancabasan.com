2b4c5e42dea65022460b422e4f0ac7a6a79e95a7
taleldayekh <mail@talel.se>
2020-02-06 20:03:50 +0100

    Finishing initial setup of react ts with ESLint and Prettier

--- src/client/.eslintrc.js
+++ src/client/.eslintrc.js
@@ -1,47 +1,48 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    'extends': [
-        'eslint:recommended',
-        'prettier',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
-        'plugin:@typescript-eslint/eslint-recommended'
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      4
     ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint'
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
