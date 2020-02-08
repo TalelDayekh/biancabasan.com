6c680b62c53541f57ac5be529aa61696719bf369
taleldayekh <mail@talel.se>
2020-02-08 15:24:54 +0100

    Trying out functional components

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import InputField from './components/InputField';
 
-
-class App extends React.PureComponent {
-    render() {
-        return (
-            <h1>Hello World</h1>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <InputField first_name={'Talel'} second_name={'Dayekh'}/>
+      </div>
+    );
+  }
 }
-
 
 export default App;
