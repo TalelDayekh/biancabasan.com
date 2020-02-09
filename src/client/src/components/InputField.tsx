47f1bad8e161a0b98402cbe2c924a1ad4742238a
taleldayekh <mail@talel.se>
2020-02-09 13:34:01 +0100

    Working configuration for scss modules

--- src/client/src/components/InputField.tsx
+++ src/client/src/components/InputField.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import styles from './InputField.module.scss';
 
 const InputField = () => {
-    return (
-        <h1 className={styles.test}>Hello World</h1>
-    )
-}
+  return <h1 className={styles.test}>Hello World</h1>;
+};
 
-export default InputField
+export default InputField;
