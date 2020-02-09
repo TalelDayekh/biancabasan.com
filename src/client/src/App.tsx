69c6250897ab92c51408eb05761cf240c988fb61
taleldayekh <mail@talel.se>
2020-02-09 13:34:01 +0100

    Working configuration for scss modules

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import InputField from './components/InputField';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <InputField/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <InputField />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -2,48 +2,46 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.module\.(scss|sass)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
             },
-            {
-                test: /\.module\.(scss|sass)$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-modules-typescript-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true
-                        }
-                    },
-                    {
-                        loader: "sass-loader",
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
-}
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
