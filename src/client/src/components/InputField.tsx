f730c5dad518ca59ef2e26af7d410dbfbb0dcaa3
taleldayekh <mail@talel.se>
2020-02-09 10:57:21 +0100

    Trying to add SCSS to React TypeScript

---
+++ src/client/src/components/InputField.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './InputField.module.scss';
+
+const InputField = () => {
+    return (
+        <h1 className={styles.test}>Hello World</h1>
+    )
+}
+
+export default InputField
