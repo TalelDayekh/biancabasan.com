cfe94772d78870dd9a2651d3a8fac71e7e587337
taleldayekh <mail@talel.se>
2020-02-22 14:27:51 +0100

    Placeholder text function and a first test

--- src/client/src/components/InputField.test.tsx
+++ src/client/src/components/InputField.test.tsx
@@ -1,2 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import InputField from './InputField';
+
+describe('InputField.tsx', () => {
+  test('renders component based on input type', () => {
+    const title = render(<InputField inputType="title" />);
+    const technique = render(<InputField inputType="technique" />);
+    const height = render(<InputField inputType="height" />);
+    const width = render(<InputField inputType="width" />);
+    const depth = render(<InputField inputType="depth" />);
+
+    expect(title.getByPlaceholderText('Title')).toBeTruthy();
+    expect(technique.getByPlaceholderText('Technique')).toBeTruthy();
+    expect(height.getByPlaceholderText('Height')).toBeTruthy();
+    expect(width.getByPlaceholderText('Width')).toBeTruthy();
+    expect(depth.getByPlaceholderText('Depth')).toBeTruthy();
+  });
+});
