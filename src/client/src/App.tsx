0862e56901ebe63b08b75c9c5984a37b01291bd3
taleldayekh <mail@talel.se>
2020-02-23 20:52:41 +0100

    Replacing measurement input type with separate for high, width and depth

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -5,15 +5,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        <InputField inputType={'title'} required hidePassword />
+        <InputField inputType={'title'} required />
         <br />
         <InputField inputType={'technique'} required />
         <br />
         <InputField inputType={'description'} required />
         <br />
-        <InputField inputType={'measurement'} shortField required />
+        <InputField inputType={'height'} shortField required />
         <br />
-        <InputField inputType={'new-password'} />
+        <InputField inputType={'width'} shortField required />
+        <br />
+        <InputField inputType={'depth'} shortField required />
+        <br />
+        <InputField inputType={'password'} />
       </div>
     );
   }
