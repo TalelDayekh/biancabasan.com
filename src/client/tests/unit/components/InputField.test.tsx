fa559e76fb8ddcbb109f903e475d0439299a1d0c
taleldayekh <mail@talel.se>
2020-03-04 20:10:14 +0100

    Reworking InputField component

--- src/client/tests/unit/components/InputField.test.tsx
+++ src/client/tests/unit/components/InputField.test.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import InputField from '../../../src/components/InputField';
 
 describe('InputField.tsx', () => {
-  test('renders component based on input type', () => {
-    const titleInput = render(<InputField inputType="title" />);
-    const techniqueInput = render(<InputField inputType="technique" />);
-    const heightInput = render(<InputField inputType="height" />);
-    const widthInput = render(<InputField inputType="width" />);
-    const depthInput = render(<InputField inputType="depth" />);
-    const descriptionInput = render(<InputField inputType="description" />);
+  let titleInput: RenderResult;
+  let techniqueInput: RenderResult;
+  let heightInput: RenderResult;
+  let widthInput: RenderResult;
+  let depthInput: RenderResult;
+  let descriptionInput: RenderResult;
 
+  beforeEach(() => {
+    titleInput = render(<InputField inputType="title" />);
+    techniqueInput = render(<InputField inputType="technique" />);
+    heightInput = render(<InputField inputType="height" />);
+    widthInput = render(<InputField inputType="width" />);
+    depthInput = render(<InputField inputType="depth" />);
+    descriptionInput = render(<InputField inputType="description" />);
+  });
+
+  test('renders component based on input type', () => {
     expect(titleInput.getByPlaceholderText('Title')).toBeTruthy();
     expect(techniqueInput.getByPlaceholderText('Technique')).toBeTruthy();
     expect(heightInput.getByPlaceholderText('Height')).toBeTruthy();
@@ -18,4 +27,22 @@ describe('InputField.tsx', () => {
     expect(depthInput.getByPlaceholderText('Depth')).toBeTruthy();
     expect(descriptionInput.getByPlaceholderText('Description')).toBeTruthy();
   });
+
+  test('clears placeholder text when input field is focused', () => {
+    titleInput.getByPlaceholderText('Title').focus();
+    techniqueInput.getByPlaceholderText('Technique').focus();
+    heightInput.getByPlaceholderText('Height').focus();
+    widthInput.getByPlaceholderText('Width').focus();
+    depthInput.getByPlaceholderText('Depth').focus();
+    descriptionInput.getByPlaceholderText('Description').focus();
+
+    expect(titleInput.getByPlaceholderText('')).toBeTruthy();
+    expect(techniqueInput.getByPlaceholderText('')).toBeTruthy();
+    expect(heightInput.getByPlaceholderText('')).toBeTruthy();
+    expect(widthInput.getByPlaceholderText('')).toBeTruthy();
+    expect(depthInput.getByPlaceholderText('')).toBeTruthy();
+    expect(descriptionInput.getByPlaceholderText('')).toBeTruthy();
+  });
+
+  test('displays error message when invalid text input is provided', () => {});
 });
