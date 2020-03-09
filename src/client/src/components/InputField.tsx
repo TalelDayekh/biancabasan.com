8cee438290fb5a37e9f1b6f21666cfd3d7c4c5d5
taleldayekh <mail@talel.se>
2020-03-09 18:09:44 +0100

    Adding back input validators for text and number

--- src/client/src/components/InputField.tsx
+++ src/client/src/components/InputField.tsx
@@ -38,9 +38,47 @@ const InputField: React.FC<InputFieldProps> = ({
       : (e.target.placeholder = placeholderText);
   };
 
-  const validateTextAndNumberInput = (): void => {
+  const validateTextAndNumberInput = (
+    e:
+      | React.FocusEvent<HTMLInputElement>
+      | React.FocusEvent<HTMLTextAreaElement>,
+  ): void => {
     let inputLength: number =
       inputType === 'title' ? 80 : inputType === 'technique' ? 255 : Infinity;
+
+    if (required && e.target.value.length <= 0) {
+      setState({ ...state, inputError: 'INPUT ERROR REQUIRED' });
+    } else if (
+      (inputType === 'height' ||
+        inputType === 'width' ||
+        inputType === 'depth') &&
+      isNaN(Number(e.target.value))
+    ) {
+      updateFormState(inputType, '');
+      setState({ ...state, inputError: 'INPUT ERROR NUMBER' });
+    } else if (e.target.value.length > inputLength) {
+      updateFormState(inputType, '');
+      setState({ ...state, inputError: 'TO LONG INPUT' });
+    }
+  };
+
+  const selectInputValidator = (
+    e:
+      | React.FocusEvent<HTMLInputElement>
+      | React.FocusEvent<HTMLTextAreaElement>,
+  ): void => {
+    switch (inputType) {
+      case 'title':
+      case 'technique':
+      case 'height':
+      case 'width':
+      case 'depth':
+      case 'description':
+        validateTextAndNumberInput(e);
+        break;
+      case 'password':
+        break;
+    }
   };
 
   if (inputType === 'description') {
@@ -57,11 +95,24 @@ const InputField: React.FC<InputFieldProps> = ({
     return (
       <>
         <input
+          className={`
+            ${styles['user-input']}
+            ${shortField && styles['short-field']}
+            ${state.inputError && styles['error']}
+          `}
           placeholder={placeholderText}
-          onFocus={placeholder}
-          onBlur={placeholder}
+          onFocus={e => {
+            placeholder(e);
+            setState({ ...state, inputError: '' });
+          }}
+          onBlur={e => {
+            placeholder(e);
+            selectInputValidator(e);
+          }}
           onChange={e => updateFormState(inputType, e.target.value)}
+          type={hidePassword ? 'password' : 'text'}
         />
+        <h1>{state.inputError}</h1>
       </>
     );
   }
